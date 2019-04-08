1f915c82feb7feb78862df500be0fdc40750a88f
inorichi <len@kanade.eu>
2019-04-07 20:08:40 -0400

    Add option to open manga details in a WebView
    (cherry picked from commit e7606e6dca48f7a20c998398b976332a904106db)

--- app/src/main/java/eu/kanade/tachiyomi/ui/manga/info/MangaWebViewController.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/manga/info/MangaWebViewController.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebView
-import android.webkit.WebViewClient
 import eu.kanade.tachiyomi.R
 import eu.kanade.tachiyomi.source.SourceManager
 import eu.kanade.tachiyomi.source.online.HttpSource
@@ -32,7 +31,6 @@ class MangaWebViewController(bundle: Bundle? = null) : BaseController(bundle) {
         val headers = source.headers.toMultimap().mapValues { it.value.getOrNull(0) ?: "" }
 
         val web = view as WebView
-        web.webViewClient = WebViewClient()
         web.settings.javaScriptEnabled = true
         web.settings.userAgentString = source.headers["User-Agent"]
         web.loadUrl(url, headers)
