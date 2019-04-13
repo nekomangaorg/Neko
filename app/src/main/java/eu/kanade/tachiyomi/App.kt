cd475e23fd1628004355c128febb8e25c0131bcb
inorichi <len@kanade.eu>
2019-04-13 09:10:44 -0400

    multidex non debug
    (cherry picked from commit ba674935f430e037420267a639b717c61bbc1e0f)

--- app/src/main/java/eu/kanade/tachiyomi/App.kt
+++ app/src/main/java/eu/kanade/tachiyomi/App.kt
@@ -32,9 +32,7 @@ open class App : Application() {
 
     override fun attachBaseContext(base: Context) {
         super.attachBaseContext(base)
-        if (BuildConfig.DEBUG) {
-            MultiDex.install(this)
-        }
+        MultiDex.install(this)
     }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
