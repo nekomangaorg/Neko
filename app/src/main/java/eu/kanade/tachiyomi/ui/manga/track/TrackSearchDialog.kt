f1c25b793bff075344637cd3a65e93e2586a5099
Jay <jays2kings@gmail.com>
2020-01-13 22:12:24 -0500

    Using swipe refresh for loading new manga into tracking
    (cherry picked from commit a8a9d699b2093d99075949773b2f8bbcf6975b66)

--- app/src/main/java/eu/kanade/tachiyomi/ui/manga/track/TrackSearchDialog.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/manga/track/TrackSearchDialog.kt
@@ -14,6 +14,7 @@ import eu.kanade.tachiyomi.data.track.TrackService
 import eu.kanade.tachiyomi.data.track.model.TrackSearch
 import eu.kanade.tachiyomi.ui.base.controller.DialogController
 import eu.kanade.tachiyomi.util.plusAssign
+import kotlinx.android.synthetic.main.track_controller.*
 import kotlinx.android.synthetic.main.track_search_dialog.view.*
 import rx.Subscription
 import rx.android.schedulers.AndroidSchedulers
@@ -134,6 +135,7 @@ class TrackSearchDialog : DialogController {
     }
 
     private fun onPositiveButtonClick() {
+        trackController.swipe_refresh.isRefreshing = true
         trackController.presenter.registerTracking(selectedItem, service)
     }
 
