dc9545570dc8ba0645dd6bf1d37ae13a4eb763ac
arkon <eugcheung94@gmail.com>
2020-02-02 22:57:15 -0500

    Migrate extension list fetch to coroutine

--- app/src/main/java/eu/kanade/tachiyomi/extension/api/ExtensionGithubApi.kt
+++ app/src/main/java/eu/kanade/tachiyomi/extension/api/ExtensionGithubApi.kt
@@ -13,12 +13,10 @@ import eu.kanade.tachiyomi.extension.model.LoadResult
 import eu.kanade.tachiyomi.extension.util.ExtensionLoader
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.network.NetworkHelper
-import eu.kanade.tachiyomi.network.asObservableSuccess
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.withContext
+import eu.kanade.tachiyomi.network.await
 import okhttp3.Response
-import rx.Observable
 import uy.kohesive.injekt.injectLazy
 import java.lang.Exception
 
@@ -26,23 +24,18 @@ internal class ExtensionGithubApi {
 
     private val network: NetworkHelper by injectLazy()
 
-    private val client get() = network.client
-
     private val gson: Gson by injectLazy()
 
-    private val repoUrl = "https://raw.githubusercontent.com/inorichi/tachiyomi-extensions/repo"
+    suspend fun findExtensions(): List<Extension.Available> {
+        val call = GET("$REPO_URL/index.json")
 
-    fun findExtensions(): Observable<List<Extension.Available>> {
-        val call = GET("$repoUrl/index.json")
-
-        return client.newCall(call).asObservableSuccess()
-                .map(::parseResponse)
+        return parseResponse(network.client.newCall(call).await())
     }
 
     suspend fun checkforUpdates(context: Context): List<Extension.Installed> {
         return withContext(Dispatchers.IO) {
-            val call = GET("$repoUrl/index.json")
-            val response = client.newCall(call).execute()
+            val call = GET("$REPO_URL/index.json")
+            val response = network.client.newCall(call).await()
 
             if (response.isSuccessful) {
                 val extensions = parseResponse(response)
@@ -80,13 +73,17 @@ internal class ExtensionGithubApi {
             val versionName = element["version"].string
             val versionCode = element["code"].int
             val lang = element["lang"].string
-            val icon = "$repoUrl/icon/${apkName.replace(".apk", ".png")}"
+            val icon = "$REPO_URL/icon/${apkName.replace(".apk", ".png")}"
 
             Extension.Available(name, pkgName, versionName, versionCode, lang, apkName, icon)
         }
     }
 
     fun getApkUrl(extension: Extension.Available): String {
-        return "$repoUrl/apk/${extension.apkName}"
+        return "$REPO_URL/apk/${extension.apkName}"
+    }
+
+    companion object {
+        private const val REPO_URL = "https://raw.githubusercontent.com/inorichi/tachiyomi-extensions/repo"
     }
 }
