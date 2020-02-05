7304de5a897fe064339cf94ea20921ef40e76eeb
Patrick Geneva <patfgeneva@gmail.com>
2020-02-05 00:50:48 -0500

    Progress bar for loading of related file.

--- app/src/main/java/eu/kanade/tachiyomi/ui/setting/SettingsRelatedController.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/setting/SettingsRelatedController.kt
@@ -1,6 +1,7 @@
 package eu.kanade.tachiyomi.ui.setting
 
 import android.app.Activity
+import android.app.Notification
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
@@ -8,7 +9,6 @@ import android.util.Log
 import androidx.preference.PreferenceScreen
 import eu.kanade.tachiyomi.R
 import eu.kanade.tachiyomi.data.database.DatabaseHelper
-import eu.kanade.tachiyomi.data.database.models.MangaRelatedImpl
 import eu.kanade.tachiyomi.util.getFilePicker
 import eu.kanade.tachiyomi.util.toast
 import kotlinx.io.InputStream
@@ -17,10 +17,11 @@ import org.json.JSONObject
 import uy.kohesive.injekt.Injekt
 import uy.kohesive.injekt.api.get
 import uy.kohesive.injekt.injectLazy
-import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
 import eu.kanade.tachiyomi.data.preference.PreferenceKeys as Keys
+import android.app.ProgressDialog
+import eu.kanade.tachiyomi.data.database.models.MangaRelatedImpl
 
 
 class SettingsRelatedController : SettingsController() {
@@ -84,6 +85,7 @@ class SettingsRelatedController : SettingsController() {
 
     }
 
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         when (requestCode) {
             RELATED_FILE_PATH_L -> if (data != null && resultCode == Activity.RESULT_OK) {
@@ -93,6 +95,7 @@ class SettingsRelatedController : SettingsController() {
 
                 // Parse the url from the specified uri
                 val context = applicationContext ?: return
+                val activity = activity ?: return
                 val selectedFile = data.data
                 val input: InputStream? = context.getContentResolver()?.openInputStream(data.data!!)
 
@@ -105,15 +108,8 @@ class SettingsRelatedController : SettingsController() {
                 //=============================================================
                 //=============================================================
 
-                // Get our current database
-                var db = Injekt.get<DatabaseHelper>()
-
-                // Delete the old related table
-                db.deleteAllRelated()
-
                 // Load everything from the buffer into our string
                 val result = input.bufferedReader().use { it.readText() }
-
                 val relatedPageResult: JSONObject
                 try {
                     relatedPageResult = JSONObject(result)
@@ -122,28 +118,65 @@ class SettingsRelatedController : SettingsController() {
                     return
                 }
 
-                // Loop through each and insert into the database
-                var counter: Long = 0
+                // Count the number of mangas we need to load
+                var totaMangas: Int = 0
                 for(key in relatedPageResult.keys()) {
-                    var related = MangaRelatedImpl()
-                    related.id = counter
-                    related.manga_id = key.toLong()
-                    related.matched_ids = relatedPageResult.getJSONObject(key).getJSONArray("m_ids").toString()
-                    related.matched_titles = relatedPageResult.getJSONObject(key).getJSONArray("m_titles").toString()
-                    related.scores = relatedPageResult.getJSONObject(key).getJSONArray("scores").toString()
-                    db.insertRelated(related).executeAsBlocking()
-                    counter++
+                    totaMangas++
                 }
-                context.toast("Loaded $counter mangas from file")
 
-                // Nice debug to see if they were inserted
-                //var mangas = db.getAllRelated().executeAsBlocking()
-                //context.toast("There are ${mangas.size} in the database")
+                // Create the progress bar
+                val progressDialog = ProgressDialog(activity)
+                progressDialog.isIndeterminate = false
+                progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL)
+                progressDialog.setCanceledOnTouchOutside(false)
+                progressDialog.setMax(totaMangas)
+                progressDialog.setMessage("Loading Related Mangas from File")
+                progressDialog.show()
 
-                // Finally, save when we last loaded the database
-                val dataFormater = SimpleDateFormat("MMM dd, yyyy HH:mm:ss zzz", Locale.getDefault())
-                val currentDate = dataFormater.format(Date())
-                preferences.relatedLastUpdated().set(context.resources.getString(R.string.pref_related_last_updated)+": "+currentDate.toString())
+                //=============================================================
+                //=============================================================
+
+                // Get our current database
+                var db = Injekt.get<DatabaseHelper>()
+
+                // Delete the old related table
+                db.deleteAllRelated()
+
+                // Now in a second thread lets insert into the database
+                // This takes some time so display it all to the user
+                Thread(Runnable {
+                    try {
+
+                        // Loop through each and insert into the database
+                        var counter: Int = 0
+                        for(key in relatedPageResult.keys()) {
+                            var related = MangaRelatedImpl()
+                            related.id = counter.toLong()
+                            related.manga_id = key.toLong()
+                            related.matched_ids = relatedPageResult.getJSONObject(key).getJSONArray("m_ids").toString()
+                            related.matched_titles = relatedPageResult.getJSONObject(key).getJSONArray("m_titles").toString()
+                            related.scores = relatedPageResult.getJSONObject(key).getJSONArray("scores").toString()
+                            db.insertRelated(related).executeAsBlocking()
+                            counter++
+                            progressDialog.setProgress(counter)
+                        }
+                        progressDialog.dismiss()
+                        context.toast("Loaded $counter mangas from file")
+
+                        // Nice debug to see if they were inserted
+                        //var mangas = db.getAllRelated().executeAsBlocking()
+                        //context.toast("There are ${mangas.size} in the database")
+
+                        // Finally, save when we last loaded the database
+                        val dataFormater = SimpleDateFormat("MMM dd, yyyy HH:mm:ss zzz", Locale.getDefault())
+                        val currentDate = dataFormater.format(Date())
+                        preferences.relatedLastUpdated().set(context.resources.getString(R.string.pref_related_last_updated)+": "+currentDate.toString())
+
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }).start()
+
 
             }
         }
