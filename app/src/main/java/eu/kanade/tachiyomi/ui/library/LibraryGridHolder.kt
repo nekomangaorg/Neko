3a1fb69ffdbb092f2be57721f9a5ecaa1ca519e9
Carlos <cargo8005@gmail.com>
2020-03-10 17:28:39 -0400

    dont force update library cover every time

--- app/src/main/java/eu/kanade/tachiyomi/ui/library/LibraryGridHolder.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/library/LibraryGridHolder.kt
@@ -39,26 +39,32 @@ class LibraryGridHolder(
 
         // Update the unread count and its visibility.
         with(unread_text) {
-            visibility = if (item.manga.unread > 0 || item.downloadCount > 0) View.VISIBLE else View.GONE
+            visibility =
+                if (item.manga.unread > 0 || item.downloadCount > 0) View.VISIBLE else View.GONE
 
             val badge = BadgeDrawable.Builder()
-                    .type(BadgeDrawable.TYPE_WITH_TWO_TEXT_COMPLEMENTARY)
-                    .badgeColor(ResourcesCompat.getColor(resources, R.color.colorPrimary, null))
-                    .text1(unreadCount)
-                    .text2(downloadCount)
-                    .padding(10f, 10f, 10f, 10f, 10f)
-                    .textSize(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 15f, resources.displayMetrics))
-                    .textColor(ResourcesCompat.getColor(resources, R.color.md_white_1000, null))
-                    .build()
+                .type(BadgeDrawable.TYPE_WITH_TWO_TEXT_COMPLEMENTARY)
+                .badgeColor(ResourcesCompat.getColor(resources, R.color.colorPrimary, null))
+                .text1(unreadCount)
+                .text2(downloadCount)
+                .padding(10f, 10f, 10f, 10f, 10f)
+                .textSize(
+                    TypedValue.applyDimension(
+                        TypedValue.COMPLEX_UNIT_DIP,
+                        15f,
+                        resources.displayMetrics
+                    )
+                )
+                .textColor(ResourcesCompat.getColor(resources, R.color.md_white_1000, null))
+                .build()
             text = badge.toSpannable()
         }
 
 // Update the cover.
-        GlideApp.with(view.context).clear(thumbnail)
         GlideApp.with(view.context)
-                .load(item.manga)
-                .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
-                .centerCrop()
-                .into(thumbnail)
+            .load(item.manga)
+            .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
+            .centerCrop()
+            .into(thumbnail)
     }
 }
