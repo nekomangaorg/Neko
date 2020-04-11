d5605d162c21dee59d0d860052415dcfca0e797b
Jay <jays2kings@gmail.com>
2020-04-11 01:59:57 -0400

    Hide Fab in latest updates

--- app/src/main/java/eu/kanade/tachiyomi/ui/catalogue/latest/LatestUpdatesController.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/catalogue/latest/LatestUpdatesController.kt
@@ -2,10 +2,13 @@ package eu.kanade.tachiyomi.ui.catalogue.latest
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
 import eu.kanade.tachiyomi.R
 import eu.kanade.tachiyomi.source.CatalogueSource
 import eu.kanade.tachiyomi.ui.catalogue.browse.BrowseCatalogueController
 import eu.kanade.tachiyomi.ui.catalogue.browse.BrowseCataloguePresenter
+import eu.kanade.tachiyomi.util.view.gone
+import kotlinx.android.synthetic.main.catalogue_controller.*
 
 /**
  * Controller that shows the latest manga from the catalogue. Inherit [BrowseCatalogueController].
@@ -15,6 +18,11 @@ class LatestUpdatesController(bundle: Bundle) : BrowseCatalogueController(bundle
     constructor(source: CatalogueSource) : this(Bundle().apply {
         putLong(SOURCE_ID_KEY, source.id)
     })
+
+    override fun onViewCreated(view: View) {
+        super.onViewCreated(view)
+        fab.gone()
+    }
 
     override fun createPresenter(): BrowseCataloguePresenter {
         return LatestUpdatesPresenter(args.getLong(SOURCE_ID_KEY))
