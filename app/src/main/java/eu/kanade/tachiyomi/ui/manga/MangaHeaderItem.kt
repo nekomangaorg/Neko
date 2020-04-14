14c2ac606724fb85c9a04e50b6070895aeaa38cb
Jay <jays2kings@gmail.com>
2020-04-13 20:10:50 -0400

    Added a tablet layout to manga details
    The 3 people who can use this you better appericate it

--- app/src/main/java/eu/kanade/tachiyomi/ui/manga/MangaHeaderItem.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/manga/MangaHeaderItem.kt
@@ -8,13 +8,15 @@ import eu.davidea.flexibleadapter.items.IFlexible
 import eu.kanade.tachiyomi.R
 import eu.kanade.tachiyomi.data.database.models.Manga
 
-class MangaHeaderItem(val manga: Manga, private val startExpanded: Boolean) :
+class MangaHeaderItem(val manga: Manga, var startExpanded: Boolean) :
     AbstractFlexibleItem<MangaHeaderHolder>() {
 
+    var isTablet = false
+    var isChapterHeader = false
     var isLocked = false
 
     override fun getLayoutRes(): Int {
-        return R.layout.manga_header_item
+        return if (isChapterHeader) R.layout.chapter_header_item else R.layout.manga_header_item
     }
 
     override fun isSelectable(): Boolean {
@@ -26,7 +28,7 @@ class MangaHeaderItem(val manga: Manga, private val startExpanded: Boolean) :
     }
 
     override fun createViewHolder(view: View, adapter: FlexibleAdapter<IFlexible<RecyclerView.ViewHolder>>): MangaHeaderHolder {
-        return MangaHeaderHolder(view, adapter as MangaDetailsAdapter, startExpanded)
+        return MangaHeaderHolder(view, adapter as MangaDetailsAdapter, startExpanded, isTablet)
     }
 
     override fun bindViewHolder(
@@ -35,7 +37,8 @@ class MangaHeaderItem(val manga: Manga, private val startExpanded: Boolean) :
         position: Int,
         payloads: MutableList<Any?>?
     ) {
-        holder.bind(this, manga)
+        if (isChapterHeader) holder.bindChapters()
+        else holder.bind(this, manga)
     }
 
     override fun equals(other: Any?): Boolean {
