6213c62e4be898a40e3018cd97d6d09444e6da46
arkon <eugcheung94@gmail.com>
2020-04-25 20:18:29 -0400

    Added actionable items to empty view
    (cherry picked from commit 0ea676467172bdd7c9bd51517d0e9dd057e6b4a8)

--- app/src/main/java/eu/kanade/tachiyomi/widget/EmptyView.kt
+++ app/src/main/java/eu/kanade/tachiyomi/widget/EmptyView.kt
@@ -2,11 +2,15 @@ package eu.kanade.tachiyomi.widget
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.View
 import android.widget.RelativeLayout
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.google.android.material.button.MaterialButton
 import eu.kanade.tachiyomi.R
 import eu.kanade.tachiyomi.util.system.getResourceColor
+import eu.kanade.tachiyomi.util.view.gone
 import eu.kanade.tachiyomi.util.view.setVectorCompat
+import eu.kanade.tachiyomi.util.view.visible
 import kotlinx.android.synthetic.main.common_view_empty.view.*
 
 class EmptyView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) :
@@ -20,17 +24,46 @@ class EmptyView @JvmOverloads constructor(context: Context, attrs: AttributeSet?
      * Hide the information view
      */
     fun hide() {
-        this.visibility = View.GONE
+        this.gone()
     }
 
+    /**
+     * Show the information view
+     * @param textResource text of information view
+     */
+    fun show(@DrawableRes drawable: Int, @StringRes textResource: Int, actions: List<Action>? = null) {
+        show(drawable, context.getString(textResource), actions)
+    }
     /**
      * Show the information view
      * @param drawable icon of information view
      * @param textResource text of information view
      */
-    fun show(drawable: Int, textResource: Int) {
+    fun show(@DrawableRes drawable: Int, message: String, actions: List<Action>? = null) {
         image_view.setVectorCompat(drawable, context.getResourceColor(android.R.attr.textColorHint))
-        text_label.text = context.getString(textResource)
-        this.visibility = View.VISIBLE
+        text_label.text = message
+
+        actions_container.removeAllViews()
+        if (!actions.isNullOrEmpty()) {
+            actions.forEach {
+                val button = (inflate(
+                    context,
+                    R.layout.material_text_button,
+                    null
+                ) as MaterialButton).apply {
+                    setText(it.resId)
+                    setOnClickListener(it.listener)
+                }
+
+                actions_container.addView(button)
+            }
+        }
+
+        this.visible()
     }
+
+    data class Action(
+        @StringRes val resId: Int,
+        val listener: OnClickListener
+    )
 }
