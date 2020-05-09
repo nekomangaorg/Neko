5aac3454255641ace8c9f4c28d3a4c59a1423524
Jay <jays2kings@gmail.com>
2020-05-09 12:52:51 -0400

    fixed not being able to go the manga details on recents if there wasn't a cover
    also covers now have a ripple effect on click
    
    (cherry picked from commit e20ea71403a4cd2a7de72dff85bb138d1e30b2f6)

--- app/src/main/java/eu/kanade/tachiyomi/ui/recents/RecentMangaHolder.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/recents/RecentMangaHolder.kt
@@ -20,7 +20,7 @@ class RecentMangaHolder(
 ) : BaseChapterHolder(view, adapter) {
 
     init {
-        cover_thumbnail?.setOnClickListener { adapter.delegate.onCoverClick(adapterPosition) }
+        card_layout?.setOnClickListener { adapter.delegate.onCoverClick(adapterPosition) }
     }
 
     fun bind(recentsType: Int) {
