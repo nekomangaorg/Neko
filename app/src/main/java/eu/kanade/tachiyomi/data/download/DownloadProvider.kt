bff32b3d436919107ec391404df700f2cacb23b0
Jay <jays2kings@gmail.com>
2020-05-11 15:44:13 -0400

    Revert "Don't rename folder if it's only a case change"
    This reverts commit df7aaf633aab40ebf15084e4063473030524a6f8.
    
    (cherry picked from commit f0b0d3d19785f33817ec67aff84d39c56fa41188)

--- app/src/main/java/eu/kanade/tachiyomi/data/download/DownloadProvider.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/download/DownloadProvider.kt
@@ -15,7 +15,6 @@ import eu.kanade.tachiyomi.source.online.utils.MdUtil
 import eu.kanade.tachiyomi.util.storage.DiskUtil
 import timber.log.Timber
 import uy.kohesive.injekt.injectLazy
-import java.util.Locale
 
 /**
  * This class is used to provide the directories where the downloads should be saved.
@@ -133,7 +132,6 @@ class DownloadProvider(private val context: Context) {
     }
 
     fun renameMangaFolder(from: String, to: String, sourceId: Long) {
-        if (from.toLowerCase(Locale.getDefault()) == to.toLowerCase(Locale.getDefault())) return
         val sourceManager by injectLazy<SourceManager>()
         val source = sourceManager.get(sourceId) ?: return
         val sourceDir = findSourceDir(source)
