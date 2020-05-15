d99f4d1faceb74e474e6187b96c66ac8890ace9e
Jay <jays2kings@gmail.com>
2020-05-15 03:54:06 -0400

    Using scope for updating library ui during global update
    since removing it from the scope broke it

--- app/src/main/java/eu/kanade/tachiyomi/ui/library/LibraryPresenter.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/library/LibraryPresenter.kt
@@ -651,7 +651,9 @@ class LibraryPresenter(
 
     /** Called when Library Service updates a manga, update the item as well */
     fun updateManga(manga: LibraryManga) {
-        getLibrary()
+        scope.launch {
+            getLibrary()
+        }
     }
 
     /** Undo the removal of the manga once in library */
