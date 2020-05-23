32aea55f424d77e78be50f288996fe04053db009
Jay <jays2kings@gmail.com>
2020-05-23 04:07:36 -0400

    Send manga/chapter/page details when sharing a chapter page
    closes #443
    Also for page_ string, removing the colon in it

--- app/src/main/java/eu/kanade/tachiyomi/ui/reader/ReaderPresenter.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/reader/ReaderPresenter.kt
@@ -572,7 +572,7 @@ class ReaderPresenter(
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribeFirst(
-                { view, file -> view.onShareImageResult(file) },
+                { view, file -> view.onShareImageResult(file, page) },
                 { _, _ -> /* Empty */ }
             )
     }
