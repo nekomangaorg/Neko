136aa506c12e5cd11b5fd13a9e12a94413387992
Patrick Geneva <patfgeneva@gmail.com>
2020-05-24 02:00:32 -0400

    Handle gzip'ed file (reduces from 21mb to only 8mb download)

--- app/src/main/java/eu/kanade/tachiyomi/data/similar/SimilarHttpService.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/similar/SimilarHttpService.kt
@@ -4,6 +4,7 @@ import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFact
 import eu.kanade.tachiyomi.network.NetworkHelper
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonObject
+import okhttp3.Interceptor
 import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Call
 import retrofit2.Retrofit
@@ -14,17 +15,32 @@ import uy.kohesive.injekt.api.get
 interface SimilarHttpService {
     companion object {
         fun create(): SimilarHttpService {
+
+            // unzip interceptor which will add the correct headers
+            val unzipInterceptor = Interceptor {
+                val res = it.proceed(it.request())
+                res.newBuilder()
+                    .header("Content-Encoding", "gzip")
+                    .header("Content-Type", "application/json")
+                    .build()
+            }
+
+            // actual builder, which will parse the underlying json file
             val contentType = "application/json".toMediaType()
             val restAdapter = Retrofit.Builder()
                 .baseUrl("https://github.com")
                 .addConverterFactory(Json.asConverterFactory(contentType))
-                .client(Injekt.get<NetworkHelper>().client)
+                .client(
+                    Injekt.get<NetworkHelper>().client
+                    .newBuilder()
+                    .addNetworkInterceptor(unzipInterceptor)
+                    .build()
+                )
                 .build()
-
             return restAdapter.create(SimilarHttpService::class.java)
         }
     }
 
-    @GET("/goldbattle/MangadexRecomendations/releases/download/v1.0.0/mangas_compressed.json")
+    @GET("/goldbattle/MangadexRecomendations/releases/download/v1.0.0/mangas_compressed.json.gz")
     fun getSimilarResults(): Call<JsonObject>
 }
