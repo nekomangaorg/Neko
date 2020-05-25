b659031a7d17f7ff113159d6e53d5d1250994989
Carlos <2092019+CarlosEsco@users.noreply.github.com>
2020-05-25 09:52:53 -0400

    release 2.0.12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         minSdkVersion(24)
         targetSdkVersion(29)
         applicationId = "tachiyomi.mangadex"
-        versionCode = 64
-        versionName = "2.0.11"
+        versionCode = 65
+        versionName = "2.0.12"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
         setProperty("archivesBaseName", "Neko")
