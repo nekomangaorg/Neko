83f12cfb7610af496c9cc96a3718b2c27e10b62b
Carlos <2092019+CarlosEsco@users.noreply.github.com>
2020-05-29 10:24:36 -0400

    update to as4

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ pluginManagement {
             if (requested.id.id.startsWith("org.jetbrains.kotlin")) {
                 useVersion("1.3.70")
             } else if (requested.id.id.equals("com.android.application")) {
-                useModule("com.android.tools.build:gradle:3.6.0")
+                useModule("com.android.tools.build:gradle:4.0.0")
             } else if (requested.id.id.equals("com.google.gms.google-services")) {
                 useModule("com.google.gms:google-services:4.3.3")
             } else if (requested.id.id.equals("com.google.gms.google-services")) {
