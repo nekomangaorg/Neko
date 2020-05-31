a0fe1f6464c2bc9d28ec941e53abf2863fed9132
Carlos <2092019+CarlosEsco@users.noreply.github.com>
2020-05-30 20:30:53 -0400

    remove shortcuts.xml plugin since shortcuts arent used anymore (#463)
    updated gradle to 6.1.1
    update android gradle to 4.0
    add buildSrc to use kotlin for versions didnt convert them all
    
    (cherry picked from commit 7d5fa5a603765326b55293c59849f05eaeca9282)

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,34 @@
+object Versions {
+    const val ACRA = "4.9.2"
+    const val CHUCKER = "3.2.0"
+    const val COIL = "0.10.1"
+    const val COROUTINES = "1.3.5"
+    const val FASTADAPTER = "5.0.0"
+    const val HYPERION = "0.9.27"
+    const val NUCLEUS = "3.0.0"
+    const val OKHTTP = "4.5.0"
+    const val OSS_LICENSE = "17.0.0"
+    const val RETROFIT = "2.7.2"
+    const val ROBO_ELECTRIC = "3.1.4"
+    const val RX_BINDING ="1.0.1"
+    const val TIMBER = "4.7.1"
+    const val WORKMANAGER = "2.3.3"
+}
+
+object AndroidVersions {
+    const val BUILD_TOOL = "29.0.3"
+    const val COMPILE_SDK = 29
+    const val MIN_SDK = 23
+    const val TARGET_SDK = 29
+    const val VERSION_CODE = 66
+    const val VERSION_NAME = "1.0.7"
+}
+
+object BuildPluginsVersion {
+    const val AGP = "4.0.0"
+    const val GOOGLE_SERVICES = "4.3.3"
+    const val KOTLIN = "1.3.72"
+    const val KOTLINTER = "2.3.1"
+    const val OSS_LICENSE = "0.10.2"
+    const val VERSIONS_PLUGIN = "0.28.0"
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,20 +6,18 @@ pluginManagement {
     }
     resolutionStrategy {
         eachPlugin {
-            if (requested.id.id.startsWith("org.jetbrains.kotlin")) {
-                useVersion("1.3.70")
-            } else if (requested.id.id.equals("com.android.application")) {
-                useModule("com.android.tools.build:gradle:4.0.0")
-            } else if (requested.id.id.equals("com.google.gms.google-services")) {
-                useModule("com.google.gms:google-services:4.3.3")
-            } else if (requested.id.id.equals("com.google.gms.google-services")) {
-                useModule("com.google.gms:google-services:4.3.3")
-            } else if (requested.id.id.equals("com.google.firebase.crashlytics")) {
-                useModule("com.google.firebase:firebase-crashlytics-gradle:2.0.0")
-            } else if (requested.id.id.equals("com.google.android.gms.oss-licenses-plugin")) {
-                useModule("com.google.android.gms:oss-licenses-plugin:0.10.2")
+            if (requested.id.id == "com.android.library") {
+                useModule("com.android.tools.build:gradle:${requested.version}")
             }
-
+            else if (requested.id.id == "com.android.application") {
+                useModule("com.android.tools.build:gradle:${requested.version}")
+            }
+            else if (requested.id.id.equals("com.google.gms.google-services")) {
+                useModule("com.google.gms:google-services:${requested.version}")
+            }
+            else if (requested.id.id.equals("com.google.android.gms.oss-licenses-plugin")) {
+                useModule("com.google.android.gms:oss-licenses-plugin:${requested.version}")
+            }
         }
     }
 }
