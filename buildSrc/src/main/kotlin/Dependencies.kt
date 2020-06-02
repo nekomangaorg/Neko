cd570feb1b14a396795c3e8e466a0b61bb7ab358
Jay <jays2kings@gmail.com>
2020-06-01 20:33:24 -0400

    Update ktlint to 2.3.2

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -28,7 +28,7 @@ object BuildPluginsVersion {
     const val AGP = "4.0.0"
     const val GOOGLE_SERVICES = "4.3.3"
     const val KOTLIN = "1.3.72"
-    const val KOTLINTER = "2.3.1"
+    const val KOTLINTER = "2.3.2"
     const val OSS_LICENSE = "0.10.2"
     const val VERSIONS_PLUGIN = "0.28.0"
 }
