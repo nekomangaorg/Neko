197a89509548310d606db06a654c56c452008d62
Carlos <2092019+CarlosEsco@users.noreply.github.com>
2020-07-08 15:38:16 -0400

    release 2.0.15.5

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -19,8 +19,8 @@ object AndroidVersions {
     const val COMPILE_SDK = 29
     const val MIN_SDK = 24
     const val TARGET_SDK = 29
-    const val VERSION_CODE = 72
-    const val VERSION_NAME = "2.0.15.4"
+    const val VERSION_CODE = 73
+    const val VERSION_NAME = "2.0.15.5"
 }
 
 object BuildPluginsVersion {
