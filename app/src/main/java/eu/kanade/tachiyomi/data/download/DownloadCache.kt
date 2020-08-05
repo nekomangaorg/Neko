abf12087a4f4a8e95a942faeb0c70a3fcf1b1f54
Carlos <2092019+CarlosEsco@users.noreply.github.com>
2020-08-05 16:15:14 -0400

    make renew more readable

--- app/src/main/java/eu/kanade/tachiyomi/data/download/DownloadCache.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/download/DownloadCache.kt
@@ -133,38 +133,36 @@ class DownloadCache(
             }
 
         val db: DatabaseHelper by injectLazy()
-        val mangas = db.getMangas().executeAsBlocking().groupBy { it.source }
+        val mangaGroupedBySource = db.getMangas().executeAsBlocking().groupBy { it.source }
 
         sourceDirs.forEach { sourceValue ->
-            val sourceMangasRaw = mangas[sourceValue.key]?.toMutableSet() ?: return@forEach
-            val sourceMangas = arrayOf(sourceMangasRaw.filter { it.favorite },
-                sourceMangasRaw.filterNot { it.favorite })
+            val sourceMangaRaw = mangaGroupedBySource[sourceValue.key]?.toMutableSet() ?: return@forEach
+            val sourceMangaPair = sourceMangaRaw.partition { it.favorite }
+
             val sourceDir = sourceValue.value
-            val mangaDirs = sourceDir.dir.listFiles().orEmpty().mapNotNull {
-                val name = it.name ?: return@mapNotNull null
-                name to MangaDirectory(it)
+
+            val mangaDirs = sourceDir.dir.listFiles().orEmpty().mapNotNull { mangaDir ->
+                val name = mangaDir.name ?: return@mapNotNull null
+                val chapterDirs = mangaDir.listFiles().orEmpty().mapNotNull { chapterFile -> chapterFile.name }.toHashSet()
+                name to MangaDirectory(mangaDir, chapterDirs)
             }.toMap()
 
-            mangaDirs.values.forEach { mangaDir ->
-                val chapterDirs =
-                    mangaDir.dir.listFiles().orEmpty().mapNotNull { it.name }.toHashSet()
-                mangaDir.files = chapterDirs
-            }
             val trueMangaDirs = mangaDirs.mapNotNull { mangaDir ->
-                val manga = sourceMangas.firstOrNull()?.find {
-                    DiskUtil.buildValidFilename(
-                        it.originalTitle
-                    ).toLowerCase() == mangaDir.key.toLowerCase() && it.source == sourceValue.key
-                } ?: sourceMangas.lastOrNull()?.find {
-                    DiskUtil.buildValidFilename(
-                        it.originalTitle
-                    ).toLowerCase() == mangaDir.key.toLowerCase() && it.source == sourceValue.key
-                }
+                val manga = findManga(sourceMangaPair.first, mangaDir.key, sourceValue.key) ?: findManga(sourceMangaPair.second, mangaDir.key, sourceValue.key)
                 val id = manga?.id ?: return@mapNotNull null
                 id to mangaDir.value.files
             }.toMap()
 
             mangaFiles.putAll(trueMangaDirs)
+        }
+    }
+
+    /**
+     * Searches a manga list and matches the given mangakey and source key
+     */
+    private fun findManga(mangaList: List<Manga>, mangaKey: String, sourceKey: Long): Manga? {
+        return mangaList.find {
+            DiskUtil.buildValidFilename(it.originalTitle).toLowerCase() == mangaKey.toLowerCase() && it.source == sourceKey
         }
     }
 
@@ -208,9 +206,10 @@ class DownloadCache(
 
     fun removeFolders(folders: List<String>, manga: Manga) {
         val id = manga.id ?: return
+        mangaFiles[id] ?: return
         for (chapter in folders) {
-            if (mangaFiles[id] != null && chapter in mangaFiles[id]!!) {
-                mangaFiles[id]?.remove(chapter)
+            if (chapter in mangaFiles[id]!!) {
+                mangaFiles[id]!!.remove(chapter)
             }
         }
     }
