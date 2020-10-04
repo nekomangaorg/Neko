f3baecc19cfc9ff989a377f113f2a5b57cd97e69
arkon <arkon@users.noreply.github.com>
2020-10-03 23:00:17 -0400

    Use Tachiyomiorg version of subsampling-scale-image-view (#610)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,7 +87,7 @@ androidExtensions {
 
 dependencies {
     // Modified dependencies
-    implementation("com.github.jays2kings:subsampling-scale-image-view:78f9664")
+    implementation("com.github.tachiyomiorg:subsampling-scale-image-view:6caf219")
     implementation("com.github.inorichi:junrar-android:634c1f5")
 
     // Android X libraries
