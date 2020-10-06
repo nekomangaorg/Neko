021716cf6417a6ed3048a0a696cd4427402c9406
Carlos <2092019+CarlosEsco@users.noreply.github.com>
2020-10-06 19:03:50 -0400

    release 2.1.3.11

--- buildSrc/src/main/kotlin/Configs.kt
+++ buildSrc/src/main/kotlin/Configs.kt
@@ -5,8 +5,8 @@ object Configs {
     const val minSdkVersion = 24
     const val targetSdkVersion = 29
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    const val versionCode = 91
-    const val versionName = "2.1.3.10"
+    const val versionCode = 92
+    const val versionName = "2.1.3.11"
 }
 
 object LegacyPluginClassPath {
