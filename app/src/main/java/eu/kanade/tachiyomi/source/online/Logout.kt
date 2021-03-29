004a449a7e7c4b7be7bf711d31c78be34826cc07
Carlos <2092019+CarlosEsco@users.noreply.github.com>
2021-03-29 14:06:34 -0400

    Update Logout.kt

--- app/src/main/java/eu/kanade/tachiyomi/source/online/Logout.kt
+++ app/src/main/java/eu/kanade/tachiyomi/source/online/Logout.kt
@@ -1,11 +1,3 @@
 package eu.kanade.tachiyomi.source.online
 
-/*
- * Copyright (C) 2020 The Neko Manga Open Source Project
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- */
-
 data class Logout(val loggedOut: Boolean, val error: String = "")
