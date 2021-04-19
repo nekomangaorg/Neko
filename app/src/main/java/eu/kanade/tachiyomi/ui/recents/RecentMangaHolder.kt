ff5e507255e11012addcc0f6ee632c27cf4a3324
Jays2Kings <jays@outlook.com>
2021-04-19 03:22:16 -0400

    Remove unused imports in RecentMangaHolder

--- app/src/main/java/eu/kanade/tachiyomi/ui/recents/RecentMangaHolder.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/recents/RecentMangaHolder.kt
@@ -7,10 +7,7 @@ import android.widget.ImageView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.isVisible
 import androidx.core.view.updateLayoutParams
-import coil.size.Precision
-import coil.size.Scale
 import eu.kanade.tachiyomi.R
-import eu.kanade.tachiyomi.data.database.models.Manga
 import eu.kanade.tachiyomi.data.download.model.Download
 import eu.kanade.tachiyomi.data.image.coil.loadLibraryManga
 import eu.kanade.tachiyomi.databinding.RecentMangaItemBinding
