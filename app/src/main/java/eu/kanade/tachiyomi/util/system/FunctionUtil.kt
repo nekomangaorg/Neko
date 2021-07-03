7484a278dd10e00e683defc88180e220f79176ea
Carlos <2092019+CarlosEsco@users.noreply.github.com>
2021-07-02 20:37:14 -0400

    change log to only show ms

--- app/src/main/java/eu/kanade/tachiyomi/util/system/FunctionUtil.kt
+++ app/src/main/java/eu/kanade/tachiyomi/util/system/FunctionUtil.kt
@@ -6,6 +6,6 @@ import kotlin.time.measureTimedValue
 inline fun <T> logTimeTaken(text: String? = null, block: () -> T): T {
     val result = measureTimedValue(block)
     XLog.tag("||NEKO-TIMER").disableStackTrace()
-        .d("${text ?: ""} took ${result.duration.inWholeMilliseconds} ms or roughly ${result.duration.inWholeSeconds} seconds")
+        .d("${text ?: ""} took ${result.duration.inWholeMilliseconds} ms")
     return result.value
 }
