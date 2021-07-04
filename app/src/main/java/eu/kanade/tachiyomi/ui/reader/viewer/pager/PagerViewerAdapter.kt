5c1e37f045ed544f73420234e0437acb53ef580d
Jays2Kings <jays@outlook.com>
2021-07-04 17:07:46 -0400

    fix format for last commit
    (cherry picked from commit acd6d5cd90604f3782e934fc013546812a38093c)

--- app/src/main/java/eu/kanade/tachiyomi/ui/reader/viewer/pager/PagerViewerAdapter.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/reader/viewer/pager/PagerViewerAdapter.kt
@@ -183,7 +183,8 @@ class PagerViewerAdapter(private val viewer: PagerViewer) : ViewPagerAdapter() {
             subItems.forEach {
                 if (it is ReaderPage) {
                     if (pagedItems.last().lastOrNull() != null &&
-                        pagedItems.last().last()?.chapter?.chapter?.id != it.chapter.chapter.id) {
+                        pagedItems.last().last()?.chapter?.chapter?.id != it.chapter.chapter.id
+                    ) {
                         pagedItems.add(mutableListOf())
                     }
                     pagedItems.last().add(it)
