25da8871990dba2a705e82fe27e5e87113bf3069
Jays2Kings <jays@outlook.com>
2021-07-04 13:21:18 -0400

    Update backup name/icon to match upstream
    (cherry picked from commit 505fa871e6f17543218b8cb9daaa13069bb6554d)

--- app/src/main/java/eu/kanade/tachiyomi/ui/setting/SettingsMainController.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/setting/SettingsMainController.kt
@@ -25,7 +25,7 @@ class SettingsMainController : SettingsController(), FloatingSearchInterface {
 
     override fun setupPreferenceScreen(screen: PreferenceScreen) = with(screen) {
         titleRes = R.string.settings
-        
+
         preference {
             iconDrawable =
                 MaterialDesignDx.Icon.gmf_tune.create(context, colorAttr = R.attr.colorOnSurface)
@@ -64,9 +64,9 @@ class SettingsMainController : SettingsController(), FloatingSearchInterface {
             onClick { navigateTo(SettingsTrackingController()) }
         }
         preference {
-            iconDrawable =
-                MaterialDesignDx.Icon.gmf_backup.create(context, colorAttr = R.attr.colorOnSurface)
-            titleRes = R.string.backup
+            iconRes = R.drawable.ic_backup_restore_24dp
+            iconTint = tintColor
+            titleRes = R.string.backup_and_restore
             onClick { navigateTo(SettingsBackupController()) }
         }
         preference {
