96f71835565eef795de89241901013341511e94f
Carlos <2092019+CarlosEsco@users.noreply.github.com>
2021-09-12 13:28:10 -0400

    release 2.6.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@ object Configs {
     const val minSdkVersion = 24
     const val targetSdkVersion = 30
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    const val versionCode = 126
-    const val versionName = "2.6.0"
+    const val versionCode = 127
+    const val versionName = "2.6.1"
 }
 
 plugins {
