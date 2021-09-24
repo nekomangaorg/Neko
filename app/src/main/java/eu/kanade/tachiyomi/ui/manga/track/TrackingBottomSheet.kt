2aa7e3f51a7d707aef2521af2857e2c87d623f4a
Jays2Kings <jays@outlook.com>
2021-09-23 21:01:04 -0400

    general improvements to the add tracking sheet
    (cherry picked from commit 9bbf96bbef4c5aaa690d2670b95559ac056fcc21)

--- app/src/main/java/eu/kanade/tachiyomi/ui/manga/track/TrackingBottomSheet.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/manga/track/TrackingBottomSheet.kt
@@ -205,7 +205,9 @@ class TrackingBottomSheet(private val controller: MangaDetailsController) :
     }
 
     private fun startTransition(duration: Long = 100) {
-        val transition = androidx.transition.AutoTransition()
+        val transition = androidx.transition.TransitionSet()
+            .addTransition(androidx.transition.ChangeBounds())
+            .addTransition(androidx.transition.Fade())
         transition.duration = duration
         val mainView = binding.root.parent as ViewGroup
         TransitionManager.endTransitions(mainView)
@@ -234,6 +236,7 @@ class TrackingBottomSheet(private val controller: MangaDetailsController) :
     }
 
     private fun search(query: String) {
+        startTransition()
         binding.searchProgress.visibility = View.VISIBLE
         binding.trackSearchRecycler.visibility = View.GONE
         setMiddleTrackView(binding.searchProgress.id)
@@ -280,9 +283,10 @@ class TrackingBottomSheet(private val controller: MangaDetailsController) :
 
     fun onSearchResultsError(error: Throwable) {
         Timber.e(error)
+        startTransition()
         setMiddleTrackView(binding.searchEmptyView.id)
-        binding.searchProgress.visibility = View.VISIBLE
-        binding.trackSearchRecycler.visibility = View.GONE
+        binding.searchProgress.isVisible = false
+        binding.trackSearchRecycler.isVisible = false
         searchItemAdapter.clear()
         binding.searchEmptyView.show(
             R.drawable.ic_search_off_24dp,
