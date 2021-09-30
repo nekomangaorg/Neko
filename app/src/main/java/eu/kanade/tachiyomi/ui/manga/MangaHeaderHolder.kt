a934d03a717f4d4eab2cbcac7be57e5544669a18
Jays2Kings <jays@outlook.com>
2021-09-29 20:23:19 -0400

    Fix a12 backdrop blur when changing covers
    (cherry picked from commit 1864268dbe29e02cbe99b6ab44ac7eb806694147)

--- app/src/main/java/eu/kanade/tachiyomi/ui/manga/MangaHeaderHolder.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/manga/MangaHeaderHolder.kt
@@ -148,16 +148,7 @@ class MangaHeaderHolder(
                 )
                 true
             }
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                backdrop.alpha = 0.2f
-                backdrop.setRenderEffect(
-                    RenderEffect.createBlurEffect(
-                        20f,
-                        20f,
-                        Shader.TileMode.MIRROR
-                    )
-                )
-            }
+            applyBlur()
             mangaCover.setOnClickListener { adapter.delegate.zoomImageFromThumb(coverCard) }
             trackButton.setOnClickListener { adapter.delegate.showTrackingSheet() }
             if (startExpanded) expandDesc()
@@ -166,6 +157,19 @@ class MangaHeaderHolder(
                 chapterLayout.isVisible = false
                 expandDesc()
             }
+        }
+    }
+
+    private fun applyBlur() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            binding?.backdrop?.alpha = 0.2f
+            binding?.backdrop?.setRenderEffect(
+                RenderEffect.createBlurEffect(
+                    20f,
+                    20f,
+                    Shader.TileMode.MIRROR
+                )
+            )
         }
     }
 
@@ -596,6 +600,7 @@ class MangaHeaderHolder(
                             Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height - yOffset)
                                 .toDrawable(itemView.resources)
                         )
+                        applyBlur()
                     }
                 )
             }
