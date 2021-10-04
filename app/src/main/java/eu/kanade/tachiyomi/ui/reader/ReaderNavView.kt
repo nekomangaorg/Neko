b1937af0ea7eb7e96b68ac768f4716905d6d699e
Jays2Kings <jays@outlook.com>
2021-10-03 22:50:30 -0400

    Fixed android 12 showing "capture more" for screenshots while the reader menu is open
    (cherry picked from commit 8a8c068e23325711f3df31721e5cf55985d29934)

--- app/src/main/java/eu/kanade/tachiyomi/ui/reader/ReaderNavView.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/reader/ReaderNavView.kt
@@ -1,7 +1,9 @@
 package eu.kanade.tachiyomi.ui.reader
 
 import android.content.Context
+import android.os.Build
 import android.util.AttributeSet
+import androidx.annotation.RequiresApi
 import androidx.constraintlayout.widget.ConstraintLayout
 
 class ReaderNavView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) :
@@ -12,5 +14,10 @@ class ReaderNavView @JvmOverloads constructor(context: Context, attrs: Attribute
 
     override fun shouldDelayChildPressedState(): Boolean {
         return true
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    override fun getScrollCaptureHint(): Int {
+        return SCROLL_CAPTURE_HINT_EXCLUDE
     }
 }
