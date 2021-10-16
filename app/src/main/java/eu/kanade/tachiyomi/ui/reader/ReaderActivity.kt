77d3a003a93da07b824aff073d79175641573af5
Jays2Kings <jays@outlook.com>
2021-10-15 20:44:13 -0400

    Revert back to only checking status bar visibility in reader
    (cherry picked from commit 58d4e3d2a4f0a9bfaa59f104edbc99f22ac602bc)

--- app/src/main/java/eu/kanade/tachiyomi/ui/reader/ReaderActivity.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/reader/ReaderActivity.kt
@@ -26,12 +26,10 @@ import android.view.animation.AnimationUtils
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.ColorUtils
-import androidx.core.graphics.Insets
 import androidx.core.view.GestureDetectorCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.WindowInsetsCompat.Type.navigationBars
 import androidx.core.view.WindowInsetsCompat.Type.statusBars
 import androidx.core.view.WindowInsetsCompat.Type.systemBars
 import androidx.core.view.WindowInsetsControllerCompat
@@ -786,9 +784,7 @@ class ReaderActivity : BaseRxActivity<ReaderPresenter>() {
         var firstPass = true
         binding.readerLayout.doOnApplyWindowInsetsCompat { _, insets, _ ->
             setNavColor(insets)
-            val hasNav = insets.getInsetsIgnoringVisibility(navigationBars()) == Insets.NONE
-            val navVis = if (hasNav) insets.isVisible(navigationBars()) else true
-            val vis = insets.isVisible(statusBars()) && navVis
+            val vis = insets.isVisible(statusBars())
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
                 if (!firstPass && lastVis != vis && preferences.fullscreen().get()) {
                     onVisibilityChange(vis)
