72499c1f4ef57fc0a35acf546da8a365465ef470
Carlos <2092019+CarlosEsco@users.noreply.github.com>
2021-12-28 21:16:42 -0500

    change token handling to mutex

--- app/src/main/java/eu/kanade/tachiyomi/network/TokenAuthenticator.kt
+++ app/src/main/java/eu/kanade/tachiyomi/network/TokenAuthenticator.kt
@@ -3,7 +3,8 @@ package eu.kanade.tachiyomi.network
 import com.elvishew.xlog.XLog
 import eu.kanade.tachiyomi.source.online.MangaDexLoginHelper
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.sync.Semaphore
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import okhttp3.Authenticator
 import okhttp3.Request
 import okhttp3.Response
@@ -12,7 +13,7 @@ import okhttp3.Route
 class TokenAuthenticator(val loginHelper: MangaDexLoginHelper) :
     Authenticator {
 
-    private val lock = Semaphore(1)
+    private val mutext = Mutex()
 
     override fun authenticate(route: Route?, response: Response): Request? {
         XLog.i("Detected Auth error ${response.code} on ${response.request.url}")
@@ -26,30 +27,29 @@ class TokenAuthenticator(val loginHelper: MangaDexLoginHelper) :
 
     fun refreshToken(loginHelper: MangaDexLoginHelper): String {
         var validated = false
+        return runBlocking {
+            mutext.withLock {
+                val checkToken =
+                    loginHelper.isAuthenticated()
+                if (checkToken) {
+                    XLog.i("Token is valid, other thread must have refreshed it")
+                    validated = true
+                }
+                if (validated.not()) {
+                    XLog.i("Token is invalid trying to refresh")
+                    validated =
+                        loginHelper.refreshToken()
+                }
 
-        runBlocking {
-            lock.acquire()
-            val checkToken =
-                loginHelper.isAuthenticated()
-            if (checkToken) {
-                XLog.i("Token is valid, other thread must have refreshed it")
-                validated = true
+                if (validated.not()) {
+                    XLog.i("Did not refresh token, trying to login")
+                    validated = loginHelper.login()
+                }
+                return@runBlocking when {
+                    validated -> "Bearer ${loginHelper.preferences.sessionToken()!!}"
+                    else -> ""
+                }
             }
-            if (validated.not()) {
-                XLog.i("Token is invalid trying to refresh")
-                validated =
-                    loginHelper.refreshToken()
-            }
-
-            if (validated.not()) {
-                XLog.i("Did not refresh token, trying to login")
-                validated = loginHelper.login()
-            }
-            lock.release()
-        }
-        return when {
-            validated -> "Bearer ${loginHelper.preferences.sessionToken()!!}"
-            else -> ""
         }
     }
 }
