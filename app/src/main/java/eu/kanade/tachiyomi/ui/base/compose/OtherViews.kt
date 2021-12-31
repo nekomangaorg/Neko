d563c76c5b9a60d851cb713ed63aa51f0d519481
Carlos <2092019+CarlosEsco@users.noreply.github.com>
2021-12-30 21:29:13 -0500

    add relationship (not backed by db) to the similar page and update its ui

---
+++ app/src/main/java/eu/kanade/tachiyomi/ui/base/compose/OtherViews.kt
@@ -0,0 +1,42 @@
+package eu.kanade.tachiyomi.ui.base.compose
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import eu.kanade.tachiyomi.ui.base.montserrat
+
+@Composable
+fun HeaderCard(text: String) {
+    Card(shape = RoundedCornerShape(4.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        elevation = 8.dp,
+        backgroundColor = MaterialTheme.colors.secondary) {
+        Text(
+            text = text,
+            fontSize = 18.sp,
+            fontFamily = montserrat,
+            fontWeight = FontWeight.Medium,
+            textAlign = TextAlign.Center,
+            color = MaterialTheme.colors.onSecondary,
+            modifier = Modifier.padding(all = 8.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun HeaderCardPreview() {
+    HeaderCard(text = "My Test header")
+}
