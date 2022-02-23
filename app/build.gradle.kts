1175ba11b5b2b2f3faac6efa0041be3ac354cac1
Carlos <2092019+CarlosEsco@users.noreply.github.com>
2022-02-22 19:02:40 -0500

    bump versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@ object Configs {
     const val minSdkVersion = 24
     const val targetSdkVersion = 30
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    const val versionCode = 140
-    const val versionName = "2.8.0.1"
+    const val versionCode = 141
+    const val versionName = "2.8.0.2"
 }
 
 plugins {
