c096145ba7b855f80dde0a2dfdd04c47d1184653
Carlos <2092019+CarlosEsco@users.noreply.github.com>
2022-05-10 04:55:58 -0400

    Move tooltip into its own components

---
+++ app/src/main/java/eu/kanade/tachiyomi/ui/base/components/ToolTipComponents.kt
@@ -0,0 +1,104 @@
+@file:Suppress("INVISIBLE_REFERENCE", "INVISIBLE_MEMBER")
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.minimumTouchTargetSize
+import androidx.compose.material3.toColor
+import androidx.compose.material3.tokens.IconButtonTokens
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
+
+/**
+ * This is a box around a CombinedClickableIcon Button, in which the long click of the button with show the tooltip
+ */
+@Composable
+fun TooltipBox(@StringRes toolTipLabel: Int, icon: ImageVector, buttonClicked: () -> Unit) {
+    val showTooltip = remember { mutableStateOf(false) }
+    CombinedClickableIconButton(
+        modifier = Modifier.iconButtonCombinedClickable(
+            toolTipLabel = toolTipLabel,
+            onClick = buttonClicked,
+            onLongClick = {
+                showTooltip.value = true
+            })
+    ) {
+        Icon(imageVector = icon,
+            contentDescription = stringResource(id = toolTipLabel))
+    }
+
+    Tooltip(showTooltip,
+        modifier = Modifier.background(MaterialTheme.colorScheme.onSurface)) {
+        Text(stringResource(id = toolTipLabel),
+            color = MaterialTheme.colorScheme.surface)
+    }
+}
+
+/**
+ * This button doesnt override clickable, and allows you to pass a combined clickable to do long, double, and normal click
+ */
+@Composable
+fun CombinedClickableIconButton(
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    content: @Composable () -> Unit,
+) {
+    Box(
+        modifier =
+        modifier
+            .minimumTouchTargetSize()
+            .size(IconButtonTokens.StateLayerSize),
+        contentAlignment = Alignment.Center
+    ) {
+        val contentColor =
+            if (enabled) {
+                IconButtonTokens.UnselectedIconColor.toColor()
+            } else {
+                IconButtonTokens.DisabledIconColor.toColor()
+                    .copy(alpha = IconButtonTokens.DisabledIconOpacity)
+            }
+        CompositionLocalProvider(LocalContentColor provides contentColor, content = content)
+    }
+}
+
+/**
+ * This button wraps combinedClickable with the remember ripple from a normal IconButton, and is to be used with the
+ * CombinedClickableIconButton
+ */
+@Composable
+fun Modifier.iconButtonCombinedClickable(
+    @StringRes toolTipLabel: Int,
+    onLongClick: (() -> Unit)? = null,
+    onDoubleClick: (() -> Unit)? = null,
+    onClick: () -> Unit,
+) = composed {
+    combinedClickable(
+        interactionSource = remember { MutableInteractionSource() },
+        indication = rememberRipple(
+            bounded = false,
+            radius = IconButtonTokens.StateLayerSize / 2
+        ),
+        onClickLabel = stringResource(id = toolTipLabel),
+        role = Role.Button,
+        onClick = onClick,
+        onLongClick = onLongClick,
+        onDoubleClick = onDoubleClick
+    )
+}
+
