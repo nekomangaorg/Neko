48bd0c27d66f1069b996867650699e45b77076d2
Jays2Kings <jays@outlook.com>
2022-05-10 22:26:58 -0400

    Make PagerPageHolder a subclass of ReaderPageImageView
    (cherry picked from commit 8dd2d2d0d44a9ef3915c70e196d7c6075b229b9f)

--- app/src/main/java/eu/kanade/tachiyomi/ui/reader/viewer/ReaderPageImageView.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/reader/viewer/ReaderPageImageView.kt
@@ -57,7 +57,7 @@ open class ReaderPageImageView @JvmOverloads constructor(
     var onScaleChanged: ((newScale: Float) -> Unit)? = null
     var onViewClicked: (() -> Unit)? = null
 
-    open fun onNeedsLandscapeZoom() {}
+    open fun onNeedsLandscapeZoom() { }
 
     @CallSuper
     open fun onImageLoaded() {
