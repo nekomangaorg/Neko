e018de6a8f53c5e3211316c9eb0d526dce41465b
Carlos <2092019+CarlosEsco@users.noreply.github.com>
2022-06-09 11:47:31 -0400

    a chapter item

---
+++ app/src/main/java/org/nekomanga/presentation/components/DownloadButton.kt
@@ -0,0 +1,73 @@
+package org.nekomanga.presentation.components
+
+import CombinedClickableIconButton
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDownward
+import androidx.compose.material.icons.filled.Cancel
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+import eu.kanade.tachiyomi.data.download.model.Download
+
+@Composable
+fun DownloadButton(buttonColor: Color, state: Download.State, progress: Float?, modifier: Modifier = Modifier) {
+    val disabledColor = MaterialTheme.colorScheme.onSurface.copy(alpha = NekoColors.disabledAlphaHighContrast)
+    val errorColor = MaterialTheme.colorScheme.errorContainer
+    val errorIconColor = MaterialTheme.colorScheme.onErrorContainer
+
+    CombinedClickableIconButton(modifier = modifier) {
+
+        val download = when (state) {
+            Download.State.CHECKED -> DownloadHolder(Icons.Filled.Check, MaterialTheme.colorScheme.surface, buttonColor, Color.Transparent)
+            Download.State.ERROR -> DownloadHolder(Icons.Filled.Cancel, errorIconColor, errorColor, Color.Transparent)
+            Download.State.QUEUE -> DownloadHolder(Icons.Filled.ArrowDownward, disabledColor, Color.Transparent, disabledColor)
+            Download.State.DOWNLOADED -> DownloadHolder(Icons.Filled.ArrowDownward, MaterialTheme.colorScheme.surface, buttonColor, Color.Transparent)
+            Download.State.DOWNLOADING -> DownloadHolder(Icons.Filled.ArrowDownward, disabledColor, Color.Transparent, buttonColor)
+            Download.State.NOT_DOWNLOADED -> DownloadHolder(Icons.Filled.ArrowDownward, buttonColor, Color.Transparent, Color.Transparent)
+        }
+
+        Surface(
+            modifier = Modifier
+                .size(28.dp),
+            shape = CircleShape,
+            color = download.backgroundColor,
+        ) {
+            Icon(
+                imageVector = download.icon, contentDescription = null,
+                modifier = Modifier
+                    .size(28.dp)
+                    .background(Color.Transparent),
+                tint = download.iconColor,
+            )
+        }
+
+        if (progress != null) {
+            if (progress < 0) {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .size(28.dp),
+                    color = download.progressColor, strokeWidth = 3.dp,
+                )
+            } else {
+                CircularProgressIndicator(
+                    progress = progress,
+                    modifier = Modifier
+                        .size(28.dp),
+                    color = download.progressColor, strokeWidth = 3.dp,
+                )
+            }
+        }
+    }
+}
+
+private data class DownloadHolder(val icon: ImageVector, val iconColor: Color, val backgroundColor: Color, val progressColor: Color)
---
+++ app/src/main/java/org/nekomanga/presentation/screens/ChapterItem.kt
@@ -0,0 +1,201 @@
+package org.nekomanga.presentation.screens
+
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Bookmark
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.crazylegend.string.isNotNullOrEmpty
+import com.google.accompanist.drawablepainter.rememberDrawablePainter
+import eu.kanade.tachiyomi.R
+import eu.kanade.tachiyomi.data.database.models.Chapter
+import eu.kanade.tachiyomi.data.download.model.Download
+import eu.kanade.tachiyomi.source.online.utils.MdLang
+import eu.kanade.tachiyomi.util.chapter.ChapterUtil
+import org.nekomanga.presentation.components.DownloadButton
+import org.nekomanga.presentation.components.NekoColors
+import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
+
+@Composable
+fun ChapterRow(chapter: Chapter, buttonColor: Color, shouldHideChapterTitles: Boolean = false, state: Download.State = Download.State.NOT_DOWNLOADED, progress: Float? = null) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { }
+            .padding(start = 16.dp, top = 8.dp, bottom = 8.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Column(modifier = Modifier.align(Alignment.CenterVertically)) {
+
+            val titleText = when (shouldHideChapterTitles) {
+                true -> stringResource(id = R.string.chapter_, decimalFormat.format(chapter.chapter_number.toDouble()))
+                false -> chapter.name
+            }
+
+            Row {
+                if (chapter.bookmark) {
+                    Icon(
+                        imageVector = Icons.Filled.Bookmark, contentDescription = null,
+                        modifier = Modifier
+                            .size(16.dp)
+                            .align(Alignment.CenterVertically),
+                        tint = buttonColor,
+                    )
+                }
+                Text(
+                    text = titleText,
+                    style = MaterialTheme.typography.bodyLarge.copy(color = MaterialTheme.colorScheme.onSurface, fontWeight = FontWeight.Medium, letterSpacing = (-.6).sp),
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                )
+            }
+
+            val statuses = mutableListOf<String>()
+
+            ChapterUtil.relativeDate(chapter)?.let { statuses.add(it) }
+
+            val showPagesLeft = !chapter.read && chapter.last_page_read > 0
+            val resources = LocalContext.current.resources
+
+            if (showPagesLeft && chapter.pages_left > 0) {
+                statuses.add(
+                    resources.getQuantityString(R.plurals.pages_left, chapter.pages_left, chapter.pages_left),
+                )
+            } else if (showPagesLeft) {
+                statuses.add(
+                    stringResource(id = R.string.page_, chapter.last_page_read + 1),
+                )
+            }
+
+            if (chapter.scanlator?.isNotBlank() == true) {
+                statuses.add(chapter.scanlator!!)
+            }
+
+            Row {
+                if (chapter.language.isNotNullOrEmpty() && chapter.language.equals("en", true).not()) {
+                    val drawable = AppCompatResources.getDrawable(LocalContext.current, MdLang.fromIsoCode(chapter.language!!)?.iconResId!!)
+                    Image(
+                        painter = rememberDrawablePainter(drawable = drawable),
+                        modifier = Modifier
+                            .height(16.dp)
+                            .clip(RoundedCornerShape(4.dp)),
+                        contentDescription = "flag",
+                    )
+                }
+                Text(
+                    text = statuses.joinToString(" • "),
+                    style = MaterialTheme.typography.bodyMedium.copy(
+                        color = MaterialTheme.colorScheme.onSurface.copy(alpha = NekoColors.mediumAlphaHighContrast),
+                        fontWeight = FontWeight.Medium,
+                        letterSpacing = (-.6).sp,
+                    ),
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                )
+
+                statuses.joinToString(" • ")
+
+            }
+
+        }
+
+
+
+        DownloadButton(buttonColor, state, progress, Modifier.align(Alignment.CenterVertically))
+    }
+}
+
+@Composable
+fun ChapterRowTester(buttonColor: Color, state: Download.State = Download.State.NOT_DOWNLOADED) {
+    Column {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.End,
+        ) {
+            Text(text = "Downloaded", color = MaterialTheme.colorScheme.onSurface)
+            DownloadButton(buttonColor, Download.State.DOWNLOADED, null, Modifier.align(Alignment.CenterVertically))
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.End,
+        ) {
+            Text(text = "Downloading", color = MaterialTheme.colorScheme.onSurface)
+            DownloadButton(buttonColor, Download.State.DOWNLOADING, 1f, Modifier.align(Alignment.CenterVertically))
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.End,
+        ) {
+            Text(text = "NotDownloaded", color = MaterialTheme.colorScheme.onSurface)
+            DownloadButton(buttonColor, Download.State.NOT_DOWNLOADED, null, Modifier.align(Alignment.CenterVertically))
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.End,
+        ) {
+            Text(text = "Queue", color = MaterialTheme.colorScheme.onSurface)
+            DownloadButton(buttonColor, Download.State.QUEUE, -1f, Modifier.align(Alignment.CenterVertically))
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.End,
+        ) {
+            Text(text = "Checked", color = MaterialTheme.colorScheme.onSurface)
+            DownloadButton(buttonColor, Download.State.CHECKED, null, Modifier.align(Alignment.CenterVertically))
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.End,
+        ) {
+            Text(text = "Error", color = MaterialTheme.colorScheme.onSurface)
+            DownloadButton(buttonColor, Download.State.ERROR, null, Modifier.align(Alignment.CenterVertically))
+        }
+    }
+}
+
+val decimalFormat = DecimalFormat(
+    "#.###",
+    DecimalFormatSymbols()
+        .apply { decimalSeparator = '.' },
+)
+
