3cfc3ba630cf25ac548f6c29998a1c0d3009ffbe
Prakash Palanisamy <51423001+RyuuPendragon@users.noreply.github.com>
2022-08-17 23:42:21 +0530

    Side padding: Added missing percentage (#1365)
    * Side padding: Added missing percentage
    
    Adding 5% side padding, as 10% side padding making webtoons thin to read.
    
    * Update strings.xml
    
    Adding missing side_padding percentage to strings
    
    (cherry picked from commit 26dae149a7f0c9e52862cde2a9f873d5f4b6b1b6)

--- app/src/main/java/eu/kanade/tachiyomi/ui/setting/SettingsReaderController.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/setting/SettingsReaderController.kt
@@ -327,12 +327,13 @@ class SettingsReaderController : SettingsController() {
                 titleRes = R.string.pref_webtoon_side_padding
                 entriesRes = arrayOf(
                     R.string.webtoon_side_padding_0,
+                    R.string.webtoon_side_padding_5,
                     R.string.webtoon_side_padding_10,
                     R.string.webtoon_side_padding_15,
                     R.string.webtoon_side_padding_20,
                     R.string.webtoon_side_padding_25,
                 )
-                entryValues = listOf(0, 10, 15, 20, 25)
+                entryValues = listOf(0, 5, 10, 15, 20, 25)
                 defaultValue = "0"
             }
 
