cb691f9051ee972f95a018b3b5fa80e7a206440a
Carlos <2092019+CarlosEsco@users.noreply.github.com>
2022-09-13 13:33:39 -0400

    slight cleanup

--- app/src/main/java/eu/kanade/tachiyomi/ui/source/browse/BrowseRepository.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/source/browse/BrowseRepository.kt
@@ -1,10 +1,11 @@
 package eu.kanade.tachiyomi.ui.source.browse
 
 import eu.kanade.tachiyomi.data.database.DatabaseHelper
-import eu.kanade.tachiyomi.data.models.DisplayManga
 import eu.kanade.tachiyomi.source.SourceManager
+import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.online.MangaDex
 import eu.kanade.tachiyomi.util.toDisplayManga
+import org.nekomanga.domain.manga.DisplayManga
 import uy.kohesive.injekt.Injekt
 import uy.kohesive.injekt.api.get
 
@@ -13,8 +14,8 @@ class BrowseRepository(
     private val db: DatabaseHelper = Injekt.get(),
 ) {
 
-    suspend fun getPage(page: Int): Pair<Boolean, List<DisplayManga>> {
-        val results = mangaDex.latestChapters(page)
+    suspend fun getPage(page: Int, query: String, filter: FilterList): Pair<Boolean, List<DisplayManga>> {
+        val results = mangaDex.search(page, query, filter)
         val displayMangaList = results.displayManga.map { sourceManga ->
             sourceManga.toDisplayManga(db, mangaDex.id)
         }
