4925e28fdf4490004372eae120b738470fbeea58
Carlos <2092019+CarlosEsco@users.noreply.github.com>
2022-09-14 13:45:04 -0400

    clean up latest

--- app/src/main/java/eu/kanade/tachiyomi/source/online/handlers/LatestChapterHandler.kt
+++ app/src/main/java/eu/kanade/tachiyomi/source/online/handlers/LatestChapterHandler.kt
@@ -4,12 +4,7 @@ import com.elvishew.xlog.XLog
 import com.github.michaelbull.result.Err
 import com.github.michaelbull.result.Ok
 import com.github.michaelbull.result.Result
-import com.skydoves.sandwich.ApiResponse
-import com.skydoves.sandwich.getOrNull
-import com.skydoves.sandwich.getOrThrow
-import com.skydoves.sandwich.isError
-import com.skydoves.sandwich.isSuccess
-import com.skydoves.sandwich.onFailure
+import com.github.michaelbull.result.andThen
 import eu.kanade.tachiyomi.data.preference.PreferencesHelper
 import eu.kanade.tachiyomi.network.NetworkHelper
 import eu.kanade.tachiyomi.network.ProxyRetrofitQueryMap
@@ -18,14 +13,11 @@ import eu.kanade.tachiyomi.source.model.MangaListPage
 import eu.kanade.tachiyomi.source.online.models.dto.ChapterListDto
 import eu.kanade.tachiyomi.source.online.utils.MdConstants
 import eu.kanade.tachiyomi.source.online.utils.MdUtil
-import eu.kanade.tachiyomi.source.online.utils.toBasicManga
-import eu.kanade.tachiyomi.util.log
-import eu.kanade.tachiyomi.util.throws
-import eu.kanade.tachiyomi.util.toResultError
+import eu.kanade.tachiyomi.source.online.utils.toSourceManga
+import eu.kanade.tachiyomi.util.getOrResultError
 import kotlinx.collections.immutable.toImmutableList
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import org.nekomanga.domain.manga.SourceManga
 import org.nekomanga.domain.network.ResultError
 import uy.kohesive.injekt.Injekt
 import uy.kohesive.injekt.api.get
@@ -49,18 +41,10 @@ class LatestChapterHandler {
 
             val response = service.latestChapters(limit, offset, langs, contentRatings)
 
-            when (response.isSuccess) {
-                true -> latestChapterParse(response.getOrNull()!!)
-                false -> {
-                    val errorType = "getting latest chapters"
-                    Err(
-                        when (response.isError) {
-                            true -> (response as ApiResponse.Failure.Error).toResultError(errorType)
-                            false -> (response as ApiResponse.Failure.Exception).toResultError(errorType)
-                        },
-                    )
+            return@withContext response.getOrResultError("getting latest chapters")
+                .andThen {
+                    latestChapterParse(it)
                 }
-            }
         }
     }
 
@@ -86,28 +70,22 @@ class LatestChapterHandler {
                     "contentRating[]" to allContentRating,
                 )
 
-            val mangaListDto = service.search(ProxyRetrofitQueryMap(queryParameters)).onFailure {
-                val type = "trying to search manga from latest chapters"
-                this.log(type)
-                this.throws(type)
-            }.getOrThrow()
 
-            val hasMoreResults = chapterListDto.limit + chapterListDto.offset < chapterListDto.total
+            service.search(ProxyRetrofitQueryMap(queryParameters))
+                .getOrResultError("trying to search manga from latest chapters").andThen { mangaListDto ->
+                    val hasMoreResults = chapterListDto.limit + chapterListDto.offset < chapterListDto.total
 
-            val mangaDtoMap = mangaListDto.data.associateBy({ it.id }, { it })
+                    val mangaDtoMap = mangaListDto.data.associateBy({ it.id }, { it })
 
-            val thumbQuality = preferencesHelper.thumbnailQuality()
-            val mangaList = mangaIds.mapNotNull { mangaDtoMap[it] }.map {
-                it.toBasicManga(thumbQuality)
-            }.map {
-                SourceManga(
-                    title = it.originalTitle,
-                    url = it.url,
-                    currentThumbnail = it.thumbnail_url ?: MdConstants.noCoverUrl,
-                )
-            }
+                    val thumbQuality = preferencesHelper.thumbnailQuality()
+                    val mangaList = mangaIds.mapNotNull { mangaDtoMap[it] }
+                        .map {
+                            it.toSourceManga(thumbQuality)
+                        }
 
-            Ok(MangaListPage(sourceManga = mangaList.toImmutableList(), hasNextPage = hasMoreResults))
+                    Ok(MangaListPage(sourceManga = mangaList.toImmutableList(), hasNextPage = hasMoreResults))
+                }
+
         }.getOrElse {
             XLog.e("Error parsing latest chapters", it)
             Err(ResultError.Generic(errorString = "Error parsing latest chapters response"))
