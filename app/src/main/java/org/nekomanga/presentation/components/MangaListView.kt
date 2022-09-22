59210a12572d87578d97ae8eeaeee30beb618366
Carlos <2092019+CarlosEsco@users.noreply.github.com>
2022-09-22 12:28:58 -0400

    MangaGrid and MangaList cleanup (#1156)

--- app/src/main/java/org/nekomanga/presentation/components/MangaListView.kt
+++ app/src/main/java/org/nekomanga/presentation/components/MangaListView.kt
@@ -1,11 +1,11 @@
 package org.nekomanga.presentation.components
 
-import androidx.compose.foundation.border
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -14,76 +14,23 @@ import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
-import com.zedlabs.pastelplaceholder.Pastel
-import eu.kanade.tachiyomi.data.image.coil.MangaCoverFetcher
-import eu.kanade.tachiyomi.util.system.toMangaCacheKey
+import java.util.Objects
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.ImmutableMap
 import org.nekomanga.domain.manga.DisplayManga
-import org.nekomanga.presentation.extensions.conditional
-import org.nekomanga.presentation.theme.Shapes
-
-@Composable
-fun MangaRow(
-    displayManga: DisplayManga,
-    shouldOutlineCover: Boolean,
-    modifier: Modifier = Modifier,
-) {
-    Row(modifier = modifier.padding(4.dp)) {
-        MangaCover(
-            displayManga,
-            shouldOutlineCover,
-            Modifier.align(alignment = Alignment.CenterVertically),
-        )
-        if (displayManga.displayText.isBlank()) {
-            MangaTitle(
-                title = displayManga.title,
-                modifier = Modifier.align(
-                    alignment = Alignment.CenterVertically,
-                ),
-                fontSize = MaterialTheme.typography.bodyLarge.fontSize,
-            )
-        } else {
-            Column(
-                Modifier
-                    .padding(4.dp)
-                    .align(
-                        alignment = Alignment.CenterVertically,
-                    ),
-            ) {
-                MangaTitle(
-                    title = displayManga.title,
-                    maxLines = 1,
-                    fontSize = MaterialTheme.typography.bodyLarge.fontSize,
-                )
-                DisplayText(
-                    displayText = displayManga.displayText,
-                    modifier = Modifier.padding(bottom = 4.dp),
-                )
-            }
-        }
-    }
-}
 
 @Composable
 fun MangaList(
-    mangaList: List<DisplayManga>,
+    mangaList: ImmutableList<DisplayManga>,
     shouldOutlineCover: Boolean = true,
     contentPadding: PaddingValues = PaddingValues(),
     onClick: (Long) -> Unit = {},
@@ -137,7 +84,7 @@ fun MangaListWithHeader(
             stickyHeader {
                 HeaderCard(stringResource(id = stringRes))
             }
-            itemsIndexed(mangaList) { _, displayManga ->
+            itemsIndexed(mangaList, key = { _, displayManga -> Objects.hash(displayManga.title, displayManga.mangaId, stringRes) }) { _, displayManga ->
                 MangaRow(
                     displayManga = displayManga,
                     shouldOutlineCover,
@@ -155,32 +102,66 @@ fun MangaListWithHeader(
 }
 
 @Composable
-private fun MangaCover(manga: DisplayManga, shouldOutlineCover: Boolean, modifier: Modifier = Modifier) {
-    Box {
-        val color by remember { mutableStateOf(Pastel.getColorLight()) }
-        AsyncImage(
-            model = ImageRequest.Builder(LocalContext.current)
-                .data(manga.currentArtwork)
-                .memoryCacheKey(manga.mangaId.toMangaCacheKey())
-                .placeholder(color)
-                .setParameter(MangaCoverFetcher.useCustomCover, false)
-                .build(),
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
+private fun MangaRow(
+    displayManga: DisplayManga,
+    shouldOutlineCover: Boolean,
+    modifier: Modifier = Modifier,
+) {
+    Row(modifier = modifier.padding(4.dp)) {
+        MangaListCover(displayManga, shouldOutlineCover)
+
+        Column(
+            modifier = Modifier
+                .padding(4.dp)
+                .align(alignment = Alignment.CenterVertically),
+        ) {
+            val titleLineCount = when (displayManga.displayText.isBlank()) {
+                true -> 2
+                false -> 1
+            }
+
+            MangaListTitle(title = displayManga.title, maxLines = titleLineCount)
+            MangaListDisplay(displayText = displayManga.displayText)
+        }
+    }
+}
+
+@Composable
+private fun MangaListTitle(title: String, maxLines: Int) {
+    Text(
+        text = title,
+        style = MaterialTheme.typography.bodyLarge,
+        color = MaterialTheme.colorScheme.onSurface,
+        maxLines = maxLines,
+        overflow = TextOverflow.Ellipsis,
+    )
+}
+
+@Composable
+private fun MangaListDisplay(displayText: String) {
+    if (displayText.isNotBlank()) {
+        Text(
+            text = displayText,
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.onSurface.copy(alpha = NekoColors.mediumAlphaLowContrast),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+        )
+    }
+}
+
+@Composable
+private fun RowScope.MangaListCover(displayManga: DisplayManga, shouldOutlineCover: Boolean) {
+    Box(modifier = Modifier.Companion.align(alignment = Alignment.CenterVertically)) {
+        MangaCover.Square.invoke(
+            manga = displayManga,
+            shouldOutlineCover = shouldOutlineCover,
             modifier = Modifier
                 .size(48.dp)
-                .padding(4.dp)
-                .clip(RoundedCornerShape(Shapes.coverRadius))
-                .conditional(shouldOutlineCover) {
-                    this.border(
-                        width = Outline.thickness,
-                        color = Outline.color,
-                        shape = RoundedCornerShape(Shapes.coverRadius),
-                    )
-                },
+                .padding(4.dp),
         )
 
-        if (manga.inLibrary) {
+        if (displayManga.inLibrary) {
             val offset = (-2).dp
             InLibraryIcon(offset, shouldOutlineCover)
         }
