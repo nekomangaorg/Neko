ef52eb6783cdc692df2901ffb64f5d49f246acd5
Carlos <2092019+CarlosEsco@users.noreply.github.com>
2022-10-05 14:01:29 -0400

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@ object Configs {
     const val minSdkVersion = 24
     const val targetSdkVersion = 30
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    const val versionCode = 161
-    const val versionName = "2.10.14"
+    const val versionCode = 162
+    const val versionName = "2.10.15"
 }
 
 fun getBuildTime() = DateTimeFormatter.ISO_DATE_TIME.format(LocalDateTime.now(ZoneOffset.UTC))
