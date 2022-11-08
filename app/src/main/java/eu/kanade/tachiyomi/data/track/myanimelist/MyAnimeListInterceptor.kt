98ad448884f7b5fc3406902c858201eff136ef70
alessandrojean <alessandrojean@users.noreply.github.com>
2022-11-08 11:50:32 -0500

    Retry the MAL request if the token is expired

--- app/src/main/java/eu/kanade/tachiyomi/data/track/myanimelist/MyAnimeListInterceptor.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/track/myanimelist/MyAnimeListInterceptor.kt
@@ -4,7 +4,6 @@ import eu.kanade.tachiyomi.network.parseAs
 import java.io.IOException
 import okhttp3.Interceptor
 import okhttp3.Response
-import okhttp3.internal.closeQuietly
 
 class MyAnimeListInterceptor(private val myanimelist: MyAnimeList, private var token: String?) : Interceptor {
 
@@ -19,29 +18,9 @@ class MyAnimeListInterceptor(private val myanimelist: MyAnimeList, private var t
         if (oauth == null) {
             oauth = myanimelist.loadOAuth()
         }
-        // Refresh access token if expired or created_at is freshly set
-        if (oauth != null &&
-            (oauth!!.isExpired() || oauth!!.created_at == System.currentTimeMillis())
-        ) {
-            val newOauth = runCatching {
-                val oauthResponse = chain.proceed(MyAnimeListApi.refreshTokenRequest(oauth!!))
-
-                if (oauthResponse.isSuccessful) {
-                    oauthResponse.parseAs<OAuth>()
-                } else {
-                    oauthResponse.closeQuietly()
-                    null
-                }
-            }
-
-            if (newOauth.getOrNull() == null) {
-                throw IOException("Failed to refresh the access token")
-            }
-
-            setAuth(newOauth.getOrNull())
-        }
-        if (oauth == null) {
-            throw IOException("No authentication token")
+        // Refresh access token if expired
+        if (oauth != null && oauth!!.isExpired()) {
+            setAuth(refreshToken(chain))
         }
 
         // Add the authorization header to the original request
@@ -49,7 +28,26 @@ class MyAnimeListInterceptor(private val myanimelist: MyAnimeList, private var t
             .addHeader("Authorization", "Bearer ${oauth!!.access_token}")
             .build()
 
-        return chain.proceed(authRequest)
+        val response = chain.proceed(authRequest)
+        val tokenIsExpired = response.headers["www-authenticate"]
+            ?.contains("The access token expired") ?: false
+
+        // Retry the request once with a new token in case it was not already refreshed
+        // by the is expired check before.
+        if (response.code == 401 && tokenIsExpired) {
+            response.close()
+
+            val newToken = refreshToken(chain)
+            setAuth(newToken)
+
+            val newRequest = originalRequest.newBuilder()
+                .addHeader("Authorization", "Bearer ${newToken.access_token}")
+                .build()
+
+            return chain.proceed(newRequest)
+        }
+
+        return response
     }
 
     /**
@@ -60,5 +58,24 @@ class MyAnimeListInterceptor(private val myanimelist: MyAnimeList, private var t
         token = oauth?.access_token
         this.oauth = oauth
         myanimelist.saveOAuth(oauth)
+    }
+
+    private fun refreshToken(chain: Interceptor.Chain): OAuth {
+        val newOauth = runCatching {
+            val oauthResponse = chain.proceed(MyAnimeListApi.refreshTokenRequest(oauth!!))
+
+            if (oauthResponse.isSuccessful) {
+                oauthResponse.parseAs<OAuth>()
+            } else {
+                oauthResponse.close()
+                null
+            }
+        }
+
+        if (newOauth.getOrNull() == null) {
+            throw IOException("Failed to refresh the access token")
+        }
+
+        return newOauth.getOrNull()!!
     }
 }
