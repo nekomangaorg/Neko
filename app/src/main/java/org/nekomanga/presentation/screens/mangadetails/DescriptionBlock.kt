c1474f4326fd00250343ea68615d55eb52b84d93
Carlos <2092019+CarlosEsco@users.noreply.github.com>
2023-01-30 08:34:43 -0500

    add selection back cause it wasnt causing the scroll issue

--- app/src/main/java/org/nekomanga/presentation/screens/mangadetails/DescriptionBlock.kt
+++ app/src/main/java/org/nekomanga/presentation/screens/mangadetails/DescriptionBlock.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.ExpandLess
@@ -157,16 +158,15 @@ fun DescriptionBlock(
             }
             val text = descriptionProvider().trim()
 
-            // SelectionContainer {
-
-            Markdown(
-                content = text,
-                colors = markdownColors(),
-                typography = markdownTypography(),
-                flavour = CommonMarkFlavourDescriptor(),
-                modifier = clickable,
-            )
-            //}
+            SelectionContainer {
+                Markdown(
+                    content = text,
+                    colors = markdownColors(),
+                    typography = markdownTypography(),
+                    flavour = CommonMarkFlavourDescriptor(),
+                    modifier = clickable,
+                )
+            }
 
             if (windowSizeClass.widthSizeClass != WindowWidthSizeClass.Expanded) {
                 Gap(8.dp)
