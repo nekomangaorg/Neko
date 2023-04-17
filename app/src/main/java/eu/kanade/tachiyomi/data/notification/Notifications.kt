cuong-tran/tachiyomiJ2K

Show an installing notification for auto app updates
Also show a button in the notification to open the app once updated
Closes #1545

app/src/main/java/eu/kanade/tachiyomi/data/notification/Notifications.kt
@@ -1,5 +1,6 @@
 package eu.kanade.tachiyomi.data.notification
 
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationChannelGroup
 import android.app.NotificationManager
@@ -21,8 +22,10 @@ object Notifications {
     const val ID_UPDATER = 1
     const val ID_DOWNLOAD_IMAGE = 2
     const val ID_INSTALL = 3
+    const val CHANNEL_INSTALLING = "installing_channel"
     const val CHANNEL_UPDATED = "updated_channel"
     const val ID_INSTALLED = -6
+    const val GROUP_APP_UPDATES = "eu.kanade.tachiyomi.APP_UPDATES"
 
     /**
      * Notification channel and ids used by the downloader.
@@ -108,6 +111,7 @@ object Notifications {
                 GROUP_EXTENSION_UPDATES,
                 context.getString(R.string.extension_updates),
             ),
+            NotificationChannelGroup(GROUP_APP_UPDATES, context.getString(R.string.app_updates)),
             NotificationChannelGroup(GROUP_LIBRARY, context.getString(R.string.library)),
         ).forEach(context.notificationManager::createNotificationChannelGroup)
 
@@ -188,7 +192,9 @@ object Notifications {
                 CHANNEL_INCOGNITO_MODE,
                 context.getString(R.string.incognito_mode),
                 NotificationManager.IMPORTANCE_LOW,
-            ),
+            ).apply {
+                lockscreenVisibility = Notification.VISIBILITY_SECRET
+            },
             NotificationChannel(
                 CHANNEL_EXT_PROGRESS,
                 context.getString(R.string.updating_extensions),
@@ -206,11 +212,22 @@ object Notifications {
                 group = GROUP_EXTENSION_UPDATES
             },
             NotificationChannel(
+                CHANNEL_INSTALLING,
+                context.getString(R.string.installing),
+                NotificationManager.IMPORTANCE_HIGH,
+            ).apply {
+                setShowBadge(false)
+                setSound(null, null)
+                enableVibration(false)
+                group = GROUP_APP_UPDATES
+            },
+            NotificationChannel(
                 CHANNEL_UPDATED,
                 context.getString(R.string.update_completed),
                 NotificationManager.IMPORTANCE_HIGH,
             ).apply {
                 setShowBadge(false)
+                group = GROUP_APP_UPDATES
             },
         )
         context.notificationManager.createNotificationChannels(channels)

nekomangaorg/Neko

(cherry picked from commit ebe8edb110ba83466eaab5ac70404772710f0aeb)

app/src/main/java/eu/kanade/tachiyomi/data/notification/Notifications.kt
@@ -1,5 +1,6 @@
 package eu.kanade.tachiyomi.data.notification
 
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationChannelGroup
 import android.app.NotificationManager
@@ -19,6 +20,7 @@ object Notifications {
         const val Tracking = "tracking_channel"
         const val Updated = "updated_channel"
         const val v5Migration = "v5_migration_channel"
+        const val Installing = "installing_channel"
         const val ID_V5_MIGRATION_PROGRESS = -901
         const val ID_V5_MIGRATION_ERROR = -902
     }
@@ -54,6 +56,9 @@ object Notifications {
     const val ID_UPDATER = 1
     const val ID_DOWNLOAD_IMAGE = 2
     const val ID_INSTALL = 3
+    const val CHANNEL_UPDATED = "updated_channel"
+    const val ID_INSTALLED = -6
+    const val GROUP_APP_UPDATES = "eu.kanade.tachiyomi.APP_UPDATES"
 
     /**
      * Notification channel and ids used by the downloader.
@@ -131,6 +136,7 @@ object Notifications {
                 context.getString(R.string.group_backup_restore),
             ),
             NotificationChannelGroup(GROUP_DOWNLOADER, context.getString(R.string.group_downloader)),
+            NotificationChannelGroup(GROUP_APP_UPDATES, context.getString(R.string.app_updates)),
         ).forEach(context.notificationManager::createNotificationChannelGroup)
 
         val channels = listOf(
@@ -242,6 +248,13 @@ object Notifications {
                 setSound(null, null)
             },
             NotificationChannel(
+                CHANNEL_INCOGNITO_MODE,
+                context.getString(R.string.incognito_mode),
+                NotificationManager.IMPORTANCE_LOW,
+            ).apply {
+                lockscreenVisibility = Notification.VISIBILITY_SECRET
+            },
+            NotificationChannel(
                 Channel.Status,
                 context.getString(R.string.sync_follows_to_library),
                 NotificationManager.IMPORTANCE_LOW,
@@ -256,6 +269,16 @@ object Notifications {
                 setShowBadge(false)
             },
             NotificationChannel(
+                Channel.Installing,
+                context.getString(R.string.install),
+                NotificationManager.IMPORTANCE_HIGH,
+            ).apply {
+                setShowBadge(false)
+                setSound(null, null)
+                enableVibration(false)
+                group = GROUP_APP_UPDATES
+            },
+            NotificationChannel(
                 CHANNEL_CRASH_LOGS,
                 context.getString(R.string.channel_crash_logs),
                 NotificationManager.IMPORTANCE_HIGH,
@@ -265,7 +288,8 @@ object Notifications {
                 context.getString(R.string.incognito_mode),
                 NotificationManager.IMPORTANCE_LOW,
             ),
-        ).forEach(context.notificationManager::createNotificationChannel)
+        )
+        context.notificationManager.createNotificationChannels(channels)
     }
 
     fun isNotificationChannelEnabled(context: Context, channelId: String?): Boolean {