3bb51295da695ad9b1d557d0b931ffedd2ba3f79
Carlos <2092019+CarlosEsco@users.noreply.github.com>
2023-04-18 09:13:41 -0400

    log when missing a mapping for mangaupdates

--- app/src/main/java/eu/kanade/tachiyomi/data/track/mangaupdates/MangaUpdatesHelper.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/track/mangaupdates/MangaUpdatesHelper.kt
@@ -1,8 +1,10 @@
-package eu.kanade.tachiyomi.data.track.mangaupdates;
+package eu.kanade.tachiyomi.data.track.mangaupdates
 
 import androidx.core.text.isDigitsOnly
 import eu.kanade.tachiyomi.data.database.models.Manga
 import eu.kanade.tachiyomi.util.manga.MangaMappings
+import eu.kanade.tachiyomi.util.system.loggycat
+import logcat.LogPriority
 import uy.kohesive.injekt.injectLazy
 
 object MangaUpdatesHelper {
@@ -11,6 +13,17 @@ object MangaUpdatesHelper {
     fun getMangaUpdatesApiId(manga: Manga): String? {
         manga.manga_updates_id?.let { id ->
             // 200591 is the last ID of the old IDs
+            return when (id.isDigitsOnly() && id.toLong() <= 200591) {
+                true -> {
+                    val newMuId = mappings.getMuNewForMuID(id)
+                    if (newMuId == null) {
+                        loggycat(LogPriority.ERROR) { "Missing MU ID mapping for id: $id" }
+                    }
+                    newMuId
+                }
+
+                false -> id.toLong(36).toString()
+            }
             return if (id.isDigitsOnly() && id.toLong() <= 200591) {
                 mappings.getMuNewForMuID(id)
             } else {
