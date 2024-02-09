202a943a154721eb2fcd64102750190667478ffa
nonproto <2092019+nonproto@users.noreply.github.com>
2024-02-09 12:48:24 -0500

    Fix incognito mode crashing app

--- app/src/main/java/org/nekomanga/App.kt
+++ app/src/main/java/org/nekomanga/App.kt
@@ -16,6 +16,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ProcessLifecycleOwner
@@ -186,7 +187,12 @@ open class App : Application(), DefaultLifecycleObserver {
 
         fun register() {
             if (!registered) {
-                registerReceiver(this, IntentFilter(ACTION_DISABLE_INCOGNITO_MODE))
+                ContextCompat.registerReceiver(
+                    this@App,
+                    this,
+                    IntentFilter(ACTION_DISABLE_INCOGNITO_MODE),
+                    ContextCompat.RECEIVER_EXPORTED,
+                )
                 registered = true
             }
         }
