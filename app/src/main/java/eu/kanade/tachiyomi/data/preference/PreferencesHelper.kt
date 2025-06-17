f75372bc632bf125dbf602666a2260e9619b11f3
nonproto <2092019+nonproto@users.noreply.github.com>
2025-06-17 10:39:03 -0400

    enable include unavailable by default

--- app/src/main/java/eu/kanade/tachiyomi/data/preference/PreferencesHelper.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/preference/PreferencesHelper.kt
@@ -220,7 +220,7 @@ class PreferencesHelper(val context: Context, val preferenceStore: PreferenceSto
 
     fun dataSaver() = this.preferenceStore.getBoolean(Keys.dataSaver, false)
 
-    fun includeUnavailable() = this.preferenceStore.getBoolean(Keys.includeUnavailable, false)
+    fun includeUnavailable() = this.preferenceStore.getBoolean(Keys.includeUnavailable, true)
 
     fun thumbnailQuality() = this.preferenceStore.getInt(Keys.thumbnailQuality, 0)
 
