5c5dc653e76ad62ef5b5f8aeda3edd9813db00b4
nonproto <2092019+nonproto@users.noreply.github.com>
2025-09-25 09:02:38 -0400

    Add missing fields

--- app/src/main/java/eu/kanade/tachiyomi/ui/library/LibraryScreenState.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/library/LibraryScreenState.kt
@@ -88,6 +88,9 @@ data class LibraryViewItem(
     val libraryDisplayMode: LibraryDisplayMode,
     val rawColumnCount: Float = 3f,
     val libraryCategoryItems: PersistentList<LibraryCategoryItem>,
+    val currentGroupBy: Int,
+    val trackMap: PersistentMap<Long, List<String>>,
+    val userCategories: PersistentList<CategoryItem>,
 )
 
 data class LibraryCategoryItem(
