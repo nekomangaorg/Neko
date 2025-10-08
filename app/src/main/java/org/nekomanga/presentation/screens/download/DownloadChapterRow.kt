2d11408c59d5b9715e0b708061807c1faa19791e
nonproto <2092019+nonproto@users.noreply.github.com>
2025-10-08 17:52:42 -0400

    ref: downlaod screen to use wave progress indicator

--- app/src/main/java/org/nekomanga/presentation/screens/download/DownloadChapterRow.kt
+++ app/src/main/java/org/nekomanga/presentation/screens/download/DownloadChapterRow.kt
@@ -15,7 +15,7 @@ import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.outlined.DeleteForever
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.LinearWavyProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ProgressIndicatorDefaults
 import androidx.compose.material3.Text
@@ -27,7 +27,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
@@ -120,28 +123,33 @@ private fun ChapterRow(
                     ),
                 maxLines = 1,
             )
-
+            val strokeWidth = with(LocalDensity.current) { Size.tiny.toPx() }
+            val stroke =
+                remember(strokeWidth) { Stroke(width = strokeWidth, cap = StrokeCap.Round) }
             when (download.chapterItem.downloadState == Download.State.QUEUE) {
                 true ->
-                    LinearProgressIndicator(
+                    LinearWavyProgressIndicator(
                         modifier = Modifier.fillMaxWidth(),
-                        progress = { download.chapterItem.downloadProgress.toFloat() / 100 },
                         color = MaterialTheme.colorScheme.secondary,
+                        progress = { download.chapterItem.downloadProgress.toFloat() / 100 },
                         trackColor = MaterialTheme.colorScheme.surfaceColorAtElevation(Size.medium),
-                        drawStopIndicator = {},
+                        stroke = stroke,
+                        trackStroke = stroke,
                     )
+
                 false -> {
                     val animatedProgress by
                         animateFloatAsState(
                             targetValue = download.chapterItem.downloadProgress.toFloat() / 100,
                             animationSpec = ProgressIndicatorDefaults.ProgressAnimationSpec,
                         )
-
-                    LinearProgressIndicator(
+                    LinearWavyProgressIndicator(
                         modifier = Modifier.fillMaxWidth(),
                         color = MaterialTheme.colorScheme.secondary,
-                        trackColor = MaterialTheme.colorScheme.surfaceColorAtElevation(Size.medium),
                         progress = { animatedProgress },
+                        trackColor = MaterialTheme.colorScheme.surfaceColorAtElevation(Size.medium),
+                        stroke = stroke,
+                        trackStroke = stroke,
                     )
                 }
             }
