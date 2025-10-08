41954468720049152e7c6789e7a60f75f95eb050
nonproto <2092019+nonproto@users.noreply.github.com>
2025-10-08 07:52:46 -0400

    ref: use material 3 pull to refresh library

--- app/src/main/java/org/nekomanga/presentation/components/PullRefresh.kt
+++ app/src/main/java/org/nekomanga/presentation/components/PullRefresh.kt
@@ -1,18 +1,15 @@
 package org.nekomanga.presentation.components
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.pullrefresh.PullRefreshIndicator
-import androidx.compose.material.pullrefresh.pullRefresh
-import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.material3.pulltorefresh.PullToRefreshDefaults
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
-import org.nekomanga.presentation.extensions.conditional
 import org.nekomanga.presentation.theme.Size
 
 @Composable
@@ -25,24 +22,26 @@ fun PullRefresh(
     contentColor: Color = MaterialTheme.colorScheme.onSecondary,
     content: @Composable () -> Unit,
 ) {
-    val state =
-        rememberPullRefreshState(
-            refreshing = refreshing,
+    val state = rememberPullToRefreshState()
+
+    if (enabled) {
+        PullToRefreshBox(
+            isRefreshing = refreshing,
             onRefresh = onRefresh,
-            refreshingOffset = indicatorOffset,
-        )
-
-    Box(Modifier.conditional(enabled) { this.pullRefresh(state, !refreshing) }) {
-        content()
-
-        Box(Modifier.padding().matchParentSize().clipToBounds()) {
-            PullRefreshIndicator(
-                refreshing = refreshing,
-                state = state,
-                modifier = Modifier.align(Alignment.TopCenter),
-                backgroundColor = backgroundColor,
-                contentColor = contentColor,
-            )
+            state = state,
+            indicator = {
+                PullToRefreshDefaults.LoadingIndicator(
+                    modifier = Modifier.align(Alignment.TopCenter),
+                    state = state,
+                    isRefreshing = refreshing,
+                    containerColor = backgroundColor,
+                    color = contentColor,
+                )
+            },
+        ) {
+            content()
         }
+    } else {
+        Box { content() }
     }
 }
