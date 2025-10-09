aad6b9ac1daaddcd5d516c495b00b10f360219f9
nonproto <2092019+nonproto@users.noreply.github.com>
2025-10-09 06:44:20 -0400

    fix: letter spacing

--- app/src/main/java/org/nekomanga/presentation/theme/Typefaces.kt
+++ app/src/main/java/org/nekomanga/presentation/theme/Typefaces.kt
@@ -10,7 +10,7 @@ import org.nekomanga.R
 object Typefaces {
     private val defaultTypography = Typography()
 
-    private const val LETTER_SPACING = -.4
+    private const val LETTER_SPACING = -.15
     val mplusRounded =
         FontFamily(
             Font(R.font.mplus_rounded1c_thin, FontWeight.Thin),
