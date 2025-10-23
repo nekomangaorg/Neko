bc318faca7d1de220f87ea729d7e0567139e2237
nonproto <2092019+nonproto@users.noreply.github.com>
2025-10-23 17:06:51 -0400

    chore: update AboutLibraries

--- app/src/main/java/eu/kanade/tachiyomi/ui/setting/LicensesController.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/setting/LicensesController.kt
@@ -1,12 +1,12 @@
 package eu.kanade.tachiyomi.ui.setting
 
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.res.stringResource
 import com.mikepenz.aboutlibraries.ui.compose.LibraryDefaults
+import com.mikepenz.aboutlibraries.ui.compose.android.produceLibraries
 import com.mikepenz.aboutlibraries.ui.compose.m3.LibrariesContainer
-import com.mikepenz.aboutlibraries.ui.compose.m3.libraryColors
 import eu.kanade.tachiyomi.ui.base.controller.BasicComposeController
 import org.nekomanga.R
 import org.nekomanga.presentation.components.NekoScaffold
@@ -16,6 +16,7 @@ import org.nekomanga.presentation.theme.Size
 class LicensesController : BasicComposeController() {
     @Composable
     override fun ScreenContent() {
+        val libraries by produceLibraries(R.raw.aboutlibraries)
         NekoScaffold(
             type = NekoScaffoldType.Title,
             onNavigationIconClicked = router::handleBack,
@@ -23,13 +24,7 @@ class LicensesController : BasicComposeController() {
             content = { contentPadding ->
                 LibrariesContainer(
                     contentPadding = contentPadding,
-                    colors =
-                        LibraryDefaults.libraryColors(
-                            backgroundColor = MaterialTheme.colorScheme.surface,
-                            contentColor = MaterialTheme.colorScheme.onSurface,
-                            badgeBackgroundColor = MaterialTheme.colorScheme.primary,
-                            badgeContentColor = MaterialTheme.colorScheme.onPrimary,
-                        ),
+                    libraries = libraries,
                     padding =
                         LibraryDefaults.libraryPadding(contentPadding = PaddingValues(Size.tiny)),
                 )
