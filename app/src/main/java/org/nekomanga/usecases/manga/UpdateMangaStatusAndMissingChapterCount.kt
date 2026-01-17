b5eff195e67a7fe37a88eca63af7cd271ca69934
D-Brox <davidfbg00@gmail.com>
2026-01-17 20:57:40 -0300

    fix: catch db exception due to chapter list race condition

--- app/src/main/java/org/nekomanga/usecases/manga/UpdateMangaStatusAndMissingChapterCount.kt
+++ app/src/main/java/org/nekomanga/usecases/manga/UpdateMangaStatusAndMissingChapterCount.kt
@@ -11,13 +11,20 @@ import eu.kanade.tachiyomi.util.getMissingChapters
 import eu.kanade.tachiyomi.util.isAvailable
 import eu.kanade.tachiyomi.util.system.executeOnIO
 import org.nekomanga.domain.chapter.toSimpleChapter
+import org.nekomanga.logging.TimberKt
 
 class UpdateMangaStatusAndMissingChapterCount(
     private val db: DatabaseHelper,
     private val downloadManager: DownloadManager,
 ) {
     suspend operator fun invoke(manga: Manga) {
-        val allChaps = db.getChapters(manga).executeOnIO()
+        // This can fail due to a race condition
+        val allChaps = try {
+            db.getChapters(manga).executeOnIO()
+        } catch(e: Exception) {
+            TimberKt.d { "Failed to get chapters: $e" }
+            listOf()
+        }
         val missingChapters =
             allChaps.map { it.toSimpleChapter()!!.toChapterItem() }.getMissingChapters().count
 
