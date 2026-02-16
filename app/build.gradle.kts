0734b5821ad25bf39250bcb081a5d0189cff8342
nonproto <2092019+nonproto@users.noreply.github.com>
2026-02-16 13:50:50 -0500

    chore: bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         minSdk = AndroidConfig.minSdkVersion
         targetSdk = AndroidConfig.targetSdkVersion
         applicationId = "org.nekomanga.neko"
-        versionCode = 56
-        versionName = "3.2.5"
+        versionCode = 57
+        versionName = "3.2.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
         setProperty("archivesBaseName", "Neko")
