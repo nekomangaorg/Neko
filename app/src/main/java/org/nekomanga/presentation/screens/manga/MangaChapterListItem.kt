ed10ec763e087924918c45fdadc6507716548947
nonproto <2092019+nonproto@users.noreply.github.com>
2026-02-19 12:02:00 -0500

    fix spacer

--- app/src/main/java/org/nekomanga/presentation/screens/manga/MangaChapterListItem.kt
+++ app/src/main/java/org/nekomanga/presentation/screens/manga/MangaChapterListItem.kt
@@ -1,10 +1,11 @@
 package org.nekomanga.presentation.screens.manga
 
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import eu.kanade.tachiyomi.ui.manga.MangaConstants.ChapterActions
-import jp.wasabeef.gap.Gap
 import kotlinx.collections.immutable.PersistentList
 import org.nekomanga.domain.chapter.ChapterItem
 import org.nekomanga.domain.chapter.ChapterMarkActions
@@ -77,6 +78,6 @@ fun MangaChapterListItem(
         )
     }
     if (listCardType != ListCardType.Bottom) {
-        Gap(Size.tiny)
+        Spacer(modifier = Modifier.size(Size.tiny))
     }
 }
