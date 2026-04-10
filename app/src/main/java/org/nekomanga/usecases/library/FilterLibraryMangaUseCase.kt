4e27278793d3cc6a66803cc53de8208eebbadfa1
nonproto <2092019+nonproto@users.noreply.github.com>
2026-04-10 05:16:08 -0400

    fix PR comments

--- app/src/main/java/org/nekomanga/usecases/library/FilterLibraryMangaUseCase.kt
+++ app/src/main/java/org/nekomanga/usecases/library/FilterLibraryMangaUseCase.kt
@@ -5,20 +5,14 @@ import org.nekomanga.domain.manga.LibraryMangaItem
 
 class FilterLibraryMangaUseCase {
     operator fun invoke(manga: LibraryMangaItem, filters: LibraryFilters): Boolean {
-        // Check Unread first (most common filter)
-        if (!filters.filterUnread.matches(manga)) return false
-
-        // Check Downloaded second (common and quick)
-        if (!filters.filterDownloaded.matches(manga)) return false
-
-        // Check the rest
-        if (!filters.filterBookmarked.matches(manga)) return false
-        if (!filters.filterCompleted.matches(manga)) return false
-        if (!filters.filterMangaType.matches(manga)) return false
-        if (!filters.filterMerged.matches(manga)) return false
-        if (!filters.filterUnavailable.matches(manga)) return false
-        if (!filters.filterTracked.matches(manga)) return false
-
-        return true // passed all checks
+        // The order of checks is intentional for performance, with more common filters first.
+        return filters.filterUnread.matches(manga) &&
+            filters.filterDownloaded.matches(manga) &&
+            filters.filterBookmarked.matches(manga) &&
+            filters.filterCompleted.matches(manga) &&
+            filters.filterMangaType.matches(manga) &&
+            filters.filterMerged.matches(manga) &&
+            filters.filterUnavailable.matches(manga) &&
+            filters.filterTracked.matches(manga)
     }
 }
