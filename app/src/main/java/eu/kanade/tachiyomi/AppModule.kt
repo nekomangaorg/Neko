96bb5fbef9610318d0c15367d78cd345248acb39
nonproto <2092019+nonproto@users.noreply.github.com>
2026-04-20 14:36:09 -0400

    tweaks

--- app/src/main/java/eu/kanade/tachiyomi/AppModule.kt
+++ app/src/main/java/eu/kanade/tachiyomi/AppModule.kt
@@ -4,9 +4,6 @@ import android.app.Application
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.work.WorkManager
-import com.google.firebase.Firebase
-import com.google.firebase.analytics.analytics
-import com.google.firebase.crashlytics.crashlytics
 import eu.kanade.tachiyomi.data.cache.ChapterCache
 import eu.kanade.tachiyomi.data.cache.CoverCache
 import eu.kanade.tachiyomi.data.download.DownloadManager
@@ -103,20 +100,11 @@ class AppModule(val app: Application) : InjektModule {
         addSingleton(app)
 
         addSingletonFactory {
-            val database =
-                Room.databaseBuilder(app, AppDatabase::class.java, AppDatabase.DATABASE_NAME)
-                    .setJournalMode(RoomDatabase.JournalMode.WRITE_AHEAD_LOGGING)
-                    .addMigrations(DatabaseMigrations.MIGRATION_45_46)
-                    .fallbackToDestructiveMigration(false)
-                    .build()
-            try {
-                val version = database.openHelper.readableDatabase.version
-                Firebase.crashlytics.setCustomKey("db_version", version)
-                Firebase.analytics.setUserProperty("db_version", version.toString())
-            } catch (e: Exception) {
-                Firebase.crashlytics.recordException(e)
-            }
-            database
+            Room.databaseBuilder(app, AppDatabase::class.java, AppDatabase.DATABASE_NAME)
+                .setJournalMode(RoomDatabase.JournalMode.WRITE_AHEAD_LOGGING)
+                .addMigrations(DatabaseMigrations.MIGRATION_45_46)
+                .fallbackToDestructiveMigration(false)
+                .build()
         }
 
         addSingletonFactory<ArtworkRepository> {
