88daad1e47962173e126cc001dbe7eef6ed621e4
nonproto <2092019+nonproto@users.noreply.github.com>
2026-07-14 08:41:39 -0400

    chore: update com.mikepenz.aboutlibraries.plugin.android to 15.0.3

--- app/src/main/java/org/nekomanga/presentation/screens/LicenseScreen.kt
+++ app/src/main/java/org/nekomanga/presentation/screens/LicenseScreen.kt
@@ -1,7 +1,6 @@
 package org.nekomanga.presentation.screens
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.TopAppBarDefaults
@@ -13,13 +12,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.mikepenz.aboutlibraries.ui.compose.LibraryDefaults
 import com.mikepenz.aboutlibraries.ui.compose.android.produceLibraries
 import com.mikepenz.aboutlibraries.ui.compose.m3.LibrariesContainer
 import org.nekomanga.R
 import org.nekomanga.presentation.components.scaffold.ChildScreenScaffold
 import org.nekomanga.presentation.screens.license.LicenseTopAppBar
-import org.nekomanga.presentation.theme.Size
 
 /**
  * LicenseScreen displays the open-source libraries used in the application.
@@ -55,7 +52,6 @@ fun LicenseScreen(windowSizeClass: WindowSizeClass, onBackPressed: () -> Unit) {
                 modifier = if (isTablet) Modifier.widthIn(max = 800.dp) else Modifier.fillMaxSize(),
                 libraries = libraries,
                 contentPadding = contentPadding,
-                padding = LibraryDefaults.libraryPadding(contentPadding = PaddingValues(Size.tiny)),
             )
         }
     }
