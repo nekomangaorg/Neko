6078574ee687abcbe48c587b4cdd352100b85408
nonproto <2092019+nonproto@users.noreply.github.com>
2026-09-04 06:08:04 -0400

    perf: replace unused BoxWithConstraints with Box in PagerPageItem

--- app/src/main/java/org/nekomanga/presentation/screens/reader/viewer/PagerPageItem.kt
+++ app/src/main/java/org/nekomanga/presentation/screens/reader/viewer/PagerPageItem.kt
@@ -6,7 +6,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.awaitEachGesture
 import androidx.compose.foundation.gestures.awaitFirstDown
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -125,7 +125,7 @@ fun PagerPageItem(
         extraPage?.chapter?.pageLoader?.retryPage(extraPage)
     }
 
-    BoxWithConstraints(
+    Box(
         modifier =
             modifier.fillMaxSize().background(backgroundColor).pointerInput(
                 viewer,
