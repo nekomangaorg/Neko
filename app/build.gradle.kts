c3ea1783cd8b77513ed335572720da458922bc55
nonproto <2092019+nonproto@users.noreply.github.com>
2026-09-10 09:16:51 -0400

    chore: bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         minSdk = AndroidConfig.minSdkVersion
         targetSdk = AndroidConfig.targetSdkVersion
         applicationId = "org.nekomanga.neko"
-        versionCode = 3701
-        versionName = "3.7.1"
+        versionCode = 3702
+        versionName = "3.7.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
